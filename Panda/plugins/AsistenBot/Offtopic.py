a890c9154246107a13b2f250844ee8888a099645
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-12-18 09:43:02 +0700

    Create Offtopic.py

---
+++ Panda/plugins/AsistenBot/Offtopic.py
@@ -0,0 +1,15 @@
+from telethon import events
+from Panda import PandaBot
+client = PandaBot.tgbot
+@client.on(events.NewMessage(pattern='/offtopic|/ot'))
+async def handler(event):
+  if event.chat_id == -1001448375234:
+    try:
+      ok = await event.get_reply_message()
+      oo = event.chat.username
+      mad = f"@{ok.sender.username} You're going OffTopic\nYou'll now get a Warn and a Ban...\n\n➥ OffTopic [Message](https://t.me/{event.chat.username}/{ok.id}) \n➥ Reporter : @{event.sender.username}\n➥ If OffTopic message is deleted\nOffTopic message = {ok.message}"
+      await client.send_message(event.chat_id, mad, link_preview=False)
+    except:
+      await event.reply("Tag a message and type #ot or #offtopic\n.\n.\n.")
+  else:
+     await event.reply("This command can't be used in this chat...")
