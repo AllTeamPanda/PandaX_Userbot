d7b51224fceee3f9adf4874a069d2fe9a64dd3ca
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-12-20 01:36:10 +0700

    Update dbsql.py

--- Panda/plugins/dbsql.py
+++ Panda/plugins/dbsql.py
@@ -5,24 +5,17 @@ import aiohttp
 import urllib3
 from Panda.sql_helper.globals import addgvar, delgvar, gvarstatus
 from . import edit_delete, edit_or_reply, pandaub
+from Panda.events import register
 
-plugin_category = "ilham"
 
 
-@pandaub.ilhammansiz_cmd(
-    pattern="getdb$",
-    command=("getdb", plugin_category),
-    info={
-        "header": "Get Database",
-        "usage": "{tr}getdb",
-    },
-)
+@register(outgoing=True, pattern="^.getdb$")
 async def getsql(event):
     var_ = event.pattern_match.group(1).upper()
     xxnx = await edit_or_reply(event, f"**Getting variable** `{var_}`")
     if var_ == "":
         return await xxnx.edit(
-            f"**Invalid Syntax !!** \n\nKetik `{cmd}getdb NAMA_VARIABLE`"
+            f"**Invalid Syntax !!** \n\nKetik `.getdb NAMA_VARIABLE`"
         )
     try:
         sql_v = gvarstatus(var_)
@@ -34,14 +27,7 @@ async def getsql(event):
     )
 
 
-@pandaub.ilhammansiz_cmd(
-    pattern="setdb$",
-    command=("setdb", plugin_category),
-    info={
-        "header": "set Database",
-        "usage": "{tr}setdb",
-    },
-)
+@register(outgoing=True, pattern="^.setdb$")
 async def setsql(event):
     hel_ = event.pattern_match.group(1)
     var_ = hel_.split(" ")[0].upper()
@@ -50,7 +36,7 @@ async def setsql(event):
     xxnx = await edit_or_reply(event, f"**Setting variable** `{var_}` **as** `{valu}`")
     if "" in (var_, valu):
         return await xxnx.edit(
-            f"**Invalid Syntax !!**\n\n**Ketik** `{cmd}setsql VARIABLE_NAME value`"
+            f"**Invalid Syntax !!**\n\n**Ketik** `.setsql VARIABLE_NAME value`"
         )
     try:
         addgvar(var_, valu)
@@ -59,20 +45,13 @@ async def setsql(event):
     await xxnx.edit(f"**Variable** `{var_}` **successfully added with value** `{valu}`")
 
 
-@pandaub.ilhammansiz_cmd(
-    pattern="deldb$",
-    command=("deldb", plugin_category),
-    info={
-        "header": "Get Database",
-        "usage": "{tr}deldb",
-    },
-)
+@register(outgoing=True, pattern="^.dwldb$")
 async def delsql(event):
     var_ = event.pattern_match.group(1).upper()
     xxnx = await edit_or_reply(event, f"**Deleting Variable** `{var_}`")
     if var_ == "":
         return await xxnx.edit(
-            f"**Invalid Syntax !!**\n\n**Ketik** `{cmd}delsql VARIABLE_NAME`"
+            f"**Invalid Syntax !!**\n\n**Ketik** `.delsql VARIABLE_NAME`"
         )
     try:
         delgvar(var_)
