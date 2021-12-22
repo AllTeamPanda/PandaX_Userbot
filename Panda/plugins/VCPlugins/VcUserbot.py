cb02f917bf461753c6010f920aaf982936691b24
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-12-22 16:20:22 +0700

    Update VcUserbot.py

--- Panda/plugins/VCPlugins/VcUserbot.py
+++ Panda/plugins/VCPlugins/VcUserbot.py
@@ -12,9 +12,26 @@
 from . import *
 from pytgcalls.exceptions import NotConnectedError
 from requests.exceptions import MissingSchema
+from telethon import events
 
 import requests
 
+def mansiez(**args):
+    """ Registers a new message. """
+    pattern = args.get("pattern", None)
+
+    r_pattern = r"^[/!]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        vcClient.add_event_handler(func, events.NewMessage(**args))
+        return func
+
+    return decorator
 
 @PandaVc_cmd("play")
 async def play_music_(event):
@@ -146,7 +163,7 @@ async def play_music_(event):
                 )
                 send_message = False
 
-@PandaVc_cmd("joinvc")
+@mansiez(pattern="/joinvc ?(.*)")
 async def join_(event):
     if len(event.text.split()) > 1:
         chat = event.text.split()[1]
