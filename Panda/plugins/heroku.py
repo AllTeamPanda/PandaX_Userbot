d3ecee34f8cd54085a7381c7b00547b2ee2c7dfb
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-12-22 11:57:22 +0700

    Update heroku.py

--- Panda/plugins/heroku.py
+++ Panda/plugins/heroku.py
@@ -25,6 +25,7 @@ heroku_api = "https://api.heroku.com"
 HEROKU_APP_NAME = Config.HEROKU_APP_NAME
 HEROKU_API_KEY = Config.HEROKU_API_KEY
 
+from Panda.sql_helper.globals import addgvar, delgvar, gvarstatus
 
 @pandaub.ilhammansiz_cmd(
     pattern="(set|get|del) var (.*)",
@@ -232,3 +233,76 @@ def prettyjson(obj, indent=2, maxlinelength=80):
         indent=indent,
     )
     return indentitems(items, indent, level=0)
+
+
+
+@pandaub.ilhammansiz_cmd(
+    pattern="getdb$",
+    command=("getdb", plugin_category),
+    info={
+        "header": "To Check dyno usage of userbot and also to know how much left.",
+        "usage": "{tr}getdb",
+    },
+)
+async def getsql(event):
+    var_ = event.pattern_match.group(1).upper()
+    xxnx = await edit_or_reply(event, f"**Getting variable** `{var_}`")
+    if var_ == "":
+        return await xxnx.edit(
+            f"**Invalid Syntax !!** \n\nKetik `{tr}getdb NAMA_VARIABLE`"
+        )
+    try:
+        sql_v = gvarstatus(var_)
+        os_v = os.environ.get(var_) or "None"
+    except Exception as e:
+        return await xxnx.edit(f"**ERROR !!**\n\n`{e}`")
+    await xxnx.edit(
+        f"**OS VARIABLE:** `{var_}`\n**OS VALUE :** `{os_v}`\n------------------\n**SQL VARIABLE:** `{var_}`\n**SQL VALUE :** `{sql_v}`\n"
+    )
+
+
+@pandaub.ilhammansiz_cmd(
+    pattern="setdb$",
+    command=("setdb", plugin_category),
+    info={
+        "header": "To Check dyno usage of userbot and also to know how much left.",
+        "usage": "{tr}setdb",
+    },
+)
+async def setsql(event):
+    hel_ = event.pattern_match.group(1)
+    var_ = hel_.split(" ")[0].upper()
+    val_ = hel_.split(" ")[1:]
+    valu = " ".join(val_)
+    xxnx = await edit_or_reply(event, f"**Setting variable** `{var_}` **as** `{valu}`")
+    if "" in (var_, valu):
+        return await xxnx.edit(
+            f"**Invalid Syntax !!**\n\n**Ketik** `{tr}setsql VARIABLE_NAME value`"
+        )
+    try:
+        addgvar(var_, valu)
+    except Exception as e:
+        return await xxnx.edit(f"**ERROR !!** \n\n`{e}`")
+    await xxnx.edit(f"**Variable** `{var_}` **successfully added with value** `{valu}`")
+
+
+@pandaub.ilhammansiz_cmd(
+    pattern="deldb$",
+    command=("deldb", plugin_category),
+    info={
+        "header": "To Check dyno usage of userbot and also to know how much left.",
+        "usage": "{tr}deldb",
+    },
+)
+async def delsql(event):
+    var_ = event.pattern_match.group(1).upper()
+    xxnx = await edit_or_reply(event, f"**Deleting Variable** `{var_}`")
+    if var_ == "":
+        return await xxnx.edit(
+            f"**Invalid Syntax !!**\n\n**Ketik** `{tr}delsql VARIABLE_NAME`"
+        )
+    try:
+        delgvar(var_)
+    except Exception as e:
+        return await xxnx.edit(f"**ERROR !!**\n\n`{e}`")
+    await xxnx.edit(f"**Deleted Variable** `{var_}`")
