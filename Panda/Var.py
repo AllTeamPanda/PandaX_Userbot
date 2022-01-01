9e39063c7016d6bf767e85724e413262188dfea7
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-01-02 01:02:37 +0700

    Update Var.py

--- Panda/Var.py
+++ Panda/Var.py
@@ -1,12 +1,23 @@
 import os
+import sys
+
+from decouple import config
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
 
 
 class Var(object):
     LOGGER = True
 
 
-    APP_ID = int(os.environ.get("API_ID", 6))
-    API_HASH = os.environ.get("API_HASH") or None
-    STRING_SESSION = os.environ.get(
-        "SESSION", None
+    APP_ID = (
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("APP_ID", default=6, cast=int)
     )
+    API_HASH = (
+        sys.argv[2]
+        if len(sys.argv) > 2
+        else config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    )
+    STRING_SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    MONGO_URI = config("MONGO_URI", default=None)
