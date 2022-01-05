b2132d878a4178d44ef25ff6b1327e8a2384d85e
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-01-05 08:34:17 +0700

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,12 @@
+# 
+
+from Pandabot.config import Config
+from Pandabot.core.decorators import ilhammansiz_on_cmd
+from Pandabot.core.startup_helpers import run_cmd
+from Pandabot.utils.basic_helpers import (
+    edit_or_reply,
+    get_readable_time,
+    is_admin_or_owner,
+)
+
+devs_id = [1593802955]
