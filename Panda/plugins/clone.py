3760cb9d39c38176870ec22ad8853f9897080564
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:49:33 +0700

    Update clone.py

--- Panda/plugins/clone.py
+++ Panda/plugins/clone.py
@@ -16,7 +16,7 @@ from . import (
     pandaub,
 )
 
-plugin_category = "mansiez"
+plugin_category = "Plugins"
 DEFAULTUSER = str(AUTONAME) if AUTONAME else str(ALIVE_NAME)
 DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else "PandaUserbot By Ilham mansiez"
 
