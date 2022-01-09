7824811cb41f50c01869cf8472f351612af78a0b
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 19:16:47 +0700

    Update spesial.py

--- Panda/plugins/spesial.py
+++ Panda/plugins/spesial.py
@@ -3,22 +3,21 @@ from . import PandaBot
 
 plugin_category = "Plugins"
 
-import random
-import shutil
-from datetime import datetime
+import asyncio
+import os
+import urllib
 
-from PIL import Image, ImageDraw, ImageFont
-from pySmartDL import SmartDL
+import requests
 
-FONT_FILE_TO_USE = "./Panda/core/Voice In My Head_080621160753.otf"
 
-Spesial = [
+spesial = [
      "https://telegra.ph/file/3a128f03b625e0f1a96d1.mp4"
      "https://telegra.ph/file/fc0d6c09d8ddf89aa9772.mp4"
      "https://telegra.ph/file/f915c7aeab2c481747163.mp4"
      "https://telegra.ph/file/58db2cd17ea842954e605.mp4"
      "https://telegra.ph/file/c76f67ab5288941e7e96a.mp4"
 ]
+
 
 
 @PandaBot.ilhammansiz_cmd(
@@ -30,26 +29,5 @@ Spesial = [
         "usage": "{tr}spesial ",
     },
 )
-async def spesial(event):
-    while True:
-        piclink = random.randint(0, len(Spesial) - 1)
-        SPESIAL = Spesial[piclink]
-        downloaded_file_name = "./DOWNLOADS/original_pic.png"
-        downloader = SmartDL(SPESIAL, downloaded_file_name, progress_bar=True)
-        downloader.start(blocking=False)
-        photo = "photo_pfp.png"
-        while not downloader.isFinished():
-            pass
-
-        shutil.copy(downloaded_file_name, photo)
-        Image.open(photo)
-        current_time = datetime.now().strftime(
-            "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n                                                   Time: %H:%M:%S \n                                                   Date: %d/%m/%y "
-        )
-        img = Image.open(photo)
-        drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((300, 450), current_time, font=fnt, fill=(255, 255, 255))
-        img.save(photo)
-        await event.client.send_file(photo)  # pylint:disable=E0602
-      
+async def pocong(event):
+    await PandaBot.send_file(random.choice(event.chat_id, spesial))
