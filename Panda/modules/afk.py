57dee7d20e797a4a64c6c08e0cb4ce2410863c3f
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:21:43 +0700

    Update afk.py

--- Panda/modules/afk.py
+++ Panda/modules/afk.py
@@ -11,7 +11,7 @@ from ..helpers.tools import media_type
 from ..helpers.utils import _format
 from . import BOTLOG, BOTLOG_CHATID
 
-plugin_category = "mansiz"
+plugin_category = "Plugins"
 
 LOGS = logging.getLogger(__name__)
 
