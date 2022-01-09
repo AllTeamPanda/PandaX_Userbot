10b68c312e9bee25924a1d466160f61642a19030
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:08:42 +0700

    Update client.py

--- Panda/core/client.py
+++ Panda/core/client.py
@@ -44,6 +44,192 @@ REGEX_ = REGEX()
 sudo_enabledcmds = sudo_enabled_cmds()
 
 class PandaUserbotSession(TelegramClient):
+    def ilhammansiz_cmd(
+        self: TelegramClient,
+        pattern: str or tuple = None,
+        info: Union[str, Dict[str, Union[str, List[str], Dict[str, str]]]]
+        or tuple = None,
+        groups_only: bool = False,
+        private_only: bool = False,
+        allow_sudo: bool = True,
+        dev: bool = True,
+        edited: bool = True,
+        forword=False,
+        disable_errors: bool = False,
+        command: str or tuple = None,
+        **kwargs,
+    ) -> callable:  # sourcery no-metrics
+        kwargs["func"] = kwargs.get("func", lambda e: e.via_bot_id is None)
+        kwargs.setdefault("forwards", forword)
+        if gvarstatus("blacklist_chats") is not None:
+            kwargs["blacklist_chats"] = True
+            kwargs["chats"] = blacklist_chats_list()
+        stack = inspect.stack()
+        previous_stack_frame = stack[1]
+        file_test = Path(previous_stack_frame.filename)
+        file_test = file_test.stem.replace(".py", "")
+        if command is not None:
+            command = list(command)
+            if not command[1] in BOT_INFO:
+                BOT_INFO.append(command[1])
+            try:
+                if file_test not in GRP_INFO[command[1]]:
+                    GRP_INFO[command[1]].append(file_test)
+            except BaseException:
+                GRP_INFO.update({command[1]: [file_test]})
+            try:
+                if command[0] not in PLG_INFO[file_test]:
+                    PLG_INFO[file_test].append(command[0])
+            except BaseException:
+                PLG_INFO.update({file_test: [command[0]]})
+            if not command[0] in CMD_INFO:
+                CMD_INFO[command[0]] = [_format_about(info)]
+        if pattern is not None:
+            if (
+                pattern.startswith(r"\#")
+                or not pattern.startswith(r"\#")
+                and pattern.startswith(r"^")
+            ):
+                REGEX_.regex1 = REGEX_.regex2 = re.compile(pattern)
+            else:
+                reg1 = "\\" + Config.COMMAND_HAND_LER
+                reg2 = "\\" + Config.SUDO_COMMAND_HAND_LER
+                devv = "\\" + Config.DEVS
+                REGEX_.regex1 = re.compile(reg1 + pattern)
+                REGEX_.regex2 = re.compile(reg2 + pattern)
+                REGEX_.dev = re.compile(devv + pattern)
+
+
+        def decorator(func):  # sourcery no-metrics
+            async def wrapper(check):
+                if groups_only and not check.is_group:
+                    await edit_delete(
+                        check, "`Saya tidak berpikir ini adalah grup.`", 10
+                    )
+                    return
+                if private_only and not check.is_private:
+                    await edit_delete(
+                        check, "`Saya tidak berpikir ini adalah Pesan.`", 10
+                    )
+                    return
+                try:
+                    await func(check)
+                except events.StopPropagation:
+                    raise events.StopPropagation
+                except KeyboardInterrupt:
+                    pass
+                except BaseException as e:
+                    LOGS.exception(e)
+                    if not disable_errors:
+                        if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+                            return
+                        date = (datetime.datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")
+                        ftext = f"\nDisclaimer:\nFile ini hanya disisipkan di sini HANYA di sini,\
+                                  \nkami hanya mencatat fakta kesalahan dan tanggal,\nKami menghormati privasi anda,\
+                                  \nyou may not report this error if you've\
+                                  \nany confidential data here, no one will see your data\
+                                  \n\n--------BEGIN USERBOT TRACEBACK LOG--------\
+                                  \nDate: {date}\nGroup ID: {str(check.chat_id)}\
+                                  \nSender ID: {str(check.sender_id)}\
+                                  \n\nEvent Trigger:\n{str(check.text)}\
+                                  \n\nTraceback info:\n{str(traceback.format_exc())}\
+                                  \n\nError text:\n{str(sys.exc_info()[1])}"
+                        new = {
+                            "error": str(sys.exc_info()[1]),
+                            "date": datetime.datetime.now(),
+                        }
+                        ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+                        command = 'git log --pretty=format:"%an: %s" -5'
+                        ftext += "\n\n\nLast 5 commits:\n"
+                        output = (await runcmd(command))[:2]
+                        result = output[0] + output[1]
+                        ftext += result
+                        pastelink = paste_text(ftext)
+                        text = "**PandaUserbot Error report**\n\n"
+                        link = "[Klik](https://t.me/TEAMSquadUserbotSupport)"
+                        text += "If you wanna you can report it"
+                        text += f"- just forward this message {link}.\n"
+                        text += (
+                            "Nothing is logged except the fact of error and date\n\n"
+                        )
+                        text += f"**Error report : ** [{new['error']}]({pastelink})"
+                        await check.client.send_message(
+                            Config.PRIVATE_GROUP_BOT_API_ID, text, link_preview=False
+                        )
+
+            from .session import PandaBot
+
+            if not func.__doc__ is None:
+                CMD_INFO[command[0]].append((func.__doc__).strip())
+            if pattern is not None:
+                if command is not None:
+                    if command[0] in LOADED_CMDS and wrapper in LOADED_CMDS[command[0]]:
+                        return None
+                    try:
+                        LOADED_CMDS[command[0]].append(wrapper)
+                    except BaseException:
+                        LOADED_CMDS.update({command[0]: [wrapper]})
+                if edited:
+                    PandaBot.add_event_handler(
+                        wrapper,
+                        MessageEdited(pattern=REGEX_.regex1, outgoing=True, **kwargs),
+                    )
+                PandaBot.add_event_handler(
+                    wrapper,
+                    NewMessage(pattern=REGEX_.regex1, outgoing=True, **kwargs),
+                )
+                if dev:
+                    if command is None or command[0] in sudo_enabledcmds:
+                        if edited:
+                            PandaBot.add_event_handler(
+                                wrapper,
+                                MessageEdited(
+                                    pattern=REGEX_.dev,
+                                    from_users=_dev_list() or DEV,
+                                    **kwargs,
+                                ),
+                            )
+                        PandaBot.add_event_handler(
+                                wrapper,
+                                NewMessage(
+                                pattern=REGEX_.dev,
+                                from_users=_dev_list() or DEV,
+                                **kwargs,
+                            ),
+                        )
+                if allow_sudo and gvarstatus("sudoenable") is not None:
+                    if command is None or command[0] in sudo_enabledcmds:
+                        if edited:
+                            PandaBot.add_event_handler(
+                                wrapper,
+                                MessageEdited(
+                                    pattern=REGEX_.regex2,
+                                    from_users=_sudousers_list(),
+                                    **kwargs,
+                                ),
+                            )
+                        PandaBot.add_event_handler(
+                            wrapper,
+                            NewMessage(
+                                pattern=REGEX_.regex2,
+                                from_users=_sudousers_list(),
+                                **kwargs,
+                            ),
+                        )
+            else:
+                if file_test in LOADED_CMDS and func in LOADED_CMDS[file_test]:
+                    return None
+                try:
+                    LOADED_CMDS[file_test].append(func)
+                except BaseException:
+                    LOADED_CMDS.update({file_test: [func]})
+                if edited:
+                    PandaBot.add_event_handler(func, events.MessageEdited(**kwargs))
+                PandaBot.add_event_handler(func, events.NewMessage(**kwargs))
+            return wrapper
+
+        return decorator
+
     def register(
         self: TelegramClient,
         pattern: str or tuple = None,
@@ -229,6 +415,8 @@ class PandaUserbotSession(TelegramClient):
             return wrapper
 
         return decorator
+
+
 
     def bot_cmd(
         self: TelegramClient,
