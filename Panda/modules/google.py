bad994507eaa91460696585ddb7f06d459d6e2f6
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:23:08 +0700

    Update google.py

--- Panda/modules/google.py
+++ Panda/modules/google.py
@@ -23,7 +23,7 @@ opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
 
-plugin_category = "ilham"
+plugin_category = "Modules"
 
 
 async def ParseSauce(googleurl):
