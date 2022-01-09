900cf864629f699217b7fb6cff7bf7e23c8bf2c2
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 10:05:43 +0700

    Update inlinebot.py

--- Panda/core/inlinebot.py
+++ Panda/core/inlinebot.py
@@ -70,7 +70,7 @@ def settingvar(dat):
     return ultr
 
 def main_menu():
-    text = f"**{CUSTOM_HELP_TEXT}**\n\n  **Pengguna :** {Config.ALIVE_NAME}\n   📗 Plugins:** {len(GRP_INFO['Plugins'])}\n**  📙 Modules: **{len(GRP_INFO['Modules'])}**\n   ☑ Commands:** {len(CMD_INFO)}\n**"
+    text = f"**{CUSTOM_HELP_TEXT}**\n\n  **Pengguna :** {Config.ALIVE_NAME}\n   Plugins:** {len(GRP_INFO['Plugins'])}\n**  Modules: **{len(GRP_INFO['Modules'])}**\n   ☑ Commands:** {len(CMD_INFO)}\n**"
     buttons = [
         (
             Button.url(
