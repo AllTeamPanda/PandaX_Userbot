6d93881c05a8cc0f6e8353ee7fd4de4a0bcb3235
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:31:11 +0700

    Update ArtPanda.py

--- Panda/plugins/ArtPanda.py
+++ Panda/plugins/ArtPanda.py
@@ -4,7 +4,7 @@ from Panda import pandaub
 
 from ..core.managers import edit_or_reply
 
-plugin_category = "ilham"
+plugin_category = "Plugins"
 
 # ===========================================================================================
 S = (
