9c5dba9f03d05d8a1e18ff40cb52a7111f24f720
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:59:02 +0700

    Update gadmin.py

--- Panda/plugins/gadmin.py
+++ Panda/plugins/gadmin.py
@@ -14,7 +14,7 @@ from ..sql_helper import gban_sql_helper as gban_sql
 from ..sql_helper.mute_sql import is_muted, mute, unmute
 from . import BOTLOG, BOTLOG_CHATID, admin_groups, get_user_from_event
 
-plugin_category = "mansiez"
+plugin_category = "Plugins"
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
