2540ed539e6bee6b4c804ab96831442b87971d5a
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-09 09:49:11 +0700

    Update climate.py

--- Panda/plugins/climate.py
+++ Panda/plugins/climate.py
@@ -15,7 +15,7 @@ from ..helpers.utils import _format
 from ..sql_helper.globals import addgvar, gvarstatus
 from . import edit_or_reply, logging, pandaub, reply_id
 
-plugin_category = "mansiez"
+plugin_category = "Plugins"
 
 LOGS = logging.getLogger(__name__)
 # Get time zone of the given country. Credits: @aragon12 and @zakaryan2004.
