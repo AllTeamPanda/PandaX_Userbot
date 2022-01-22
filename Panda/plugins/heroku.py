35d2e730fcb10db759216a5a739790f31f6d8dd7
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-22 22:35:50 +0700

    Update heroku.py

--- Panda/plugins/heroku.py
+++ Panda/plugins/heroku.py
@@ -15,7 +15,7 @@ from Panda import pandaub
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 
-plugin_category = "Plugins"
+plugin_category = "plugins"
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # =================
