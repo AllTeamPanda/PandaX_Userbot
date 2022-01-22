b278b205d1b3a55d91abea37f6be7a5baf5f17c7
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-22 23:22:27 +0700

    Update admin.py

--- Panda/plugins/admin.py
+++ Panda/plugins/admin.py
@@ -1,5 +1,10 @@
 from asyncio import sleep
-
+import io
+import os
+import re
+import sys
+import urllib
+from os import environ, execle
 from telethon import functions
 from telethon.errors import (
     BadRequestError,
@@ -66,6 +71,25 @@ UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 
 plugin_category = "plugins"
 # ================================================
+
+
+@pandaub.ilhammansiz_cmd(
+    pattern="restart$",
+    command=("restart", plugin_category),
+    info={
+        "header": "Merestart bot",
+        "usage": "{tr}restart",
+    },
+)
+async def restart_bot(event):
+    await edit_or_reply(event, "**PandaUserbot Berhasil di Restart**")
+    if BOTLOG_CHATID:
+        await event.client.send_message(
+            BOTLOG_CHATID, "#RESTART \n" "**PandaUserbot Berhasil Di Restart**"
+        )
+    args = [sys.executable, "-m", "Panda"]
+    execle(sys.executable, *args, environ)
+
 
 
 @pandaub.ilhammansiz_cmd(
