ff8a1bf47295553aca32f038dffe451fa2e87c71
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-01-27 00:27:23 +0700

    Update VcUserbot.py

--- Panda/plugins/VCPlugins/VcUserbot.py
+++ Panda/plugins/VCPlugins/VcUserbot.py
@@ -35,19 +35,7 @@ def mansiez(**args):
     return decorator
 
 
-plugin_category = "plugins"
-
-
-@vcClient.ilhammansiz_cmd(
-    pattern="play(?: |$)(.*)",
-    command=("play", plugin_category),
-    info={
-        "header": "Play the song in voice chat",
-        "description": "Play the song in voice chat, or add the song to queue..",
-        "usage": "{tr}play <link youtube/song name>",
-        "examples": ["{tr}play"],
-    },
-)
+@PandaVc_cmd("play")
 async def play_music_(event):
     if "playfrom" in event.text.split()[0]:
         return  # For PlayFrom Conflict
