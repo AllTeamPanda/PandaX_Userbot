4a607d5bdbbe01f771e2196768158788e0f173f4
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-02 08:49:56 +0700

    Create filters.py

---
+++ Panda/plugins/filters.py
@@ -0,0 +1,199 @@
+import os
+import re
+
+from telethon import Button, events
+
+from traceback import format_exc
+import json
+import math
+import aiohttp
+from telegraph import upload_file as uf
+from telethon.tl.types import User
+from telethon.utils import pack_bot_file_id
+from Panda.utils.tools import mediainfo
+from ..sql_helper.globals import add_filter, get_filter, list_filter, rem_filter, gvarstatus
+from Panda import PandaBot
+from telethon.tl.types import InputWebDocument, Message
+from telethon.utils import resolve_bot_file_id
+from Panda.Config import Config
+from Panda.utils.vceor import eor, eod
+from PandaXBahasa import get_string
+plugin_category = "modules"
+
+def get_msg_button(texts: str):
+    btn = []
+    for z in re.findall("\\[(.*?)\\|(.*?)\\]", texts):
+        text, url = z
+        urls = url.split("|")
+        url = urls[0]
+        # if not is_url_ok(url):
+        #    continue
+        if len(urls) > 1:
+            btn[-1].append([text, url])
+        else:
+            btn.append([[text, url]])
+
+    txt = texts
+    for z in re.findall("\\[.+?\\|.+?\\]", texts):
+        txt = txt.replace(z, "")
+
+    return txt.strip(), btn
+
+
+def create_tl_btn(button: list):
+    btn = []
+    for z in button:
+        if len(z) > 1:
+            kk = [Button.url(x, y.strip()) for x, y in z]
+            btn.append(kk)
+        else:
+            btn.append([Button.url(z[0][0], z[0][1].strip())])
+    return btn
+
+
+def format_btn(buttons: list):
+    txt = ""
+    for i in buttons:
+        a = 0
+        for i in i:
+            if hasattr(i.button, "url"):
+                a += 1
+                if a > 1:
+                    txt += f"[{i.button.text} | {i.button.url} | same]"
+                else:
+                    txt += f"[{i.button.text} | {i.button.url}]"
+    _, btn = get_msg_button(txt)
+    return btn
+
+
+STUFF = {}
+
+async def something(e, msg, media, button, reply=True, chat=None):
+    if e.client._bot:
+        return await e.reply(msg, file=media, buttons=button)
+    num = len(STUFF) + 1
+    STUFF.update({num: {"msg": msg, "media": media, "button": button}})
+    try:
+        res = await e.client.inline_query(Config.TG_BOT_USERNAME, f"stf{num}")
+        return await res[0].click(
+            chat or e.chat_id,
+            reply_to=bool(isinstance(e, Message) and reply),
+            hide_via=True,
+            silent=True,
+        )
+
+    except Exception as er:
+        LOGS.exception(er)
+
+
+
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="addfilter (.*)",
+    command=("addfilter", plugin_category),
+    info={
+        "header": "To save filter for the given keyword.",
+        "description": "If any user sends that filter then your bot will reply.",
+        "note": "For saving media/stickers as filters you need to set PRIVATE_GROUP_BOT_API_ID.",
+        "usage": "{tr}addfilter <word><reply to a message>",
+    },
+)
+async def af(e):
+    wrd = (e.pattern_match.group(1)).lower()
+    wt = await e.get_reply_message()
+    chat = e.chat_id
+    if not (wt and wrd):
+        return await e.eor(get_string("flr_1"))
+    btn = format_btn(wt.buttons) if wt.buttons else None
+    if wt and wt.media:
+        wut = mediainfo(wt.media)
+        if wut.startswith(("pic", "gif")):
+            dl = await wt.download_media()
+            variable = uf(dl)
+            m = "https://telegra.ph" + variable[0]
+        elif wut == "video":
+            if wt.media.document.size > 8 * 1000 * 1000:
+                return await e.eor(get_string("com_4"), time=5)
+            dl = await wt.download_media()
+            variable = uf(dl)
+            os.remove(dl)
+            m = "https://telegra.ph" + variable[0]
+        else:
+            m = pack_bot_file_id(wt.media)
+        if wt.text:
+            txt = wt.text
+            if not btn:
+                txt, btn = get_msg_button(wt.text)
+            add_filter(chat, wrd, txt, m, btn)
+        else:
+            add_filter(chat, wrd, None, m, btn)
+    else:
+        txt = wt.text
+        if not btn:
+            txt, btn = get_msg_button(wt.text)
+        add_filter(chat, wrd, txt, None, btn)
+    await e.eor(get_string("flr_4").format(wrd))
+    PandaBot.add_handler(filter_func, events.NewMessage())
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="remfilters$",
+    command=("remfilters", plugin_category),
+    info={
+        "header": "To delete all filters in that group.",
+        "usage": "{tr}remfilters",
+    },
+)
+async def rf(e):
+    wrd = (e.pattern_match.group(1)).lower()
+    chat = e.chat_id
+    if not wrd:
+        return await e.eor(get_string("flr_3"))
+    rem_filter(int(chat), wrd)
+    await e.eor(get_string("flr_5").format(wrd))
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="listfilter (.*)",
+    command=("listfilter", plugin_category),
+    info={
+        "header": "To delete that filter . so if user send that keyword bot will not reply",
+        "usage": "{tr}listfilter <keyword>",
+    },
+)
+async def lsnote(e):
+    x = list_filter(e.chat_id)
+    if x:
+        sd = "Filters Found In This Chats Are\n\n"
+        return await e.eor(sd + x)
+    await e.eor(get_string("flr_6"))
+
+
+async def filter_func(e):
+    if isinstance(e.sender, User) and e.sender.bot:
+        return
+    xx = (e.text).lower()
+    chat = e.chat_id
+    x = get_filter(chat)
+    if x:
+        for c in x:
+            pat = r"( |^|[^\w])" + re.escape(c) + r"( |$|[^\w])"
+            if re.search(pat, xx):
+                k = x.get(c)
+                if k:
+                    msg = k["msg"]
+                    media = k["media"]
+                    if k.get("button"):
+                        btn = create_tl_btn(k["button"])
+                        return await something(e, msg, media, btn)
+                    await e.reply(msg, file=media)
+
+
+if gvarstatus("FILTERS"):
+    PandaBot.add_handler(filter_func, events.NewMessage())
+
+
+
+
+
