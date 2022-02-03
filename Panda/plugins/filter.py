a7721ad26212e19a6674d74fd40e3335433f651c
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-04 00:38:51 +0700

    Update filter.py

--- Panda/plugins/filter.py
+++ Panda/plugins/filter.py
@@ -1,7 +1,9 @@
 # ported from paperplaneExtended by avinashreddy3108 for media support
 import re
 
-from Panda import pandaub
+from telethon.utils import get_display_name
+
+from Panda import PandaBot
 
 from ..core.managers import edit_or_reply
 from ..sql_helper.filter_sql import (
@@ -15,82 +17,133 @@ from . import BOTLOG, BOTLOG_CHATID
 plugin_category = "plugins"
 
 
-@pandaub.ilhammansiz_cmd(incoming=True)
-async def filter_incoming_handler(handler):
-    try:
-        if (
-            not (await handler.get_sender()).bot
-            and (handler.sender_id) != handler.client.uid
-        ):
-            name = handler.raw_text
-            filters = get_filters(handler.chat_id)
-            if not filters:
-                return
-            for trigger in filters:
-                pattern = r"( |^|[^\w])" + re.escape(trigger.keyword) + r"( |$|[^\w])"
-                if re.search(pattern, name, flags=re.IGNORECASE):
-                    if trigger.f_mesg_id:
-                        msg_o = await handler.client.get_messages(
-                            entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id)
-                        )
-                        await handler.reply(msg_o.message, file=msg_o.media)
-                    elif trigger.reply:
-                        await handler.reply(trigger.reply)
-    except AttributeError:
-        pass
+@PandaBot.ilhammansiz_cmd(incoming=True)
+async def filter_incoming_handler(event):  # sourcery no-metrics
+    if event.sender_id == event.client.uid:
+        return
+    name = event.raw_text
+    filters = get_filters(event.chat_id)
+    if not filters:
+        return
+    a_user = await event.get_sender()
+    chat = await event.get_chat()
+    me = await event.client.get_me()
+    title = get_display_name(await event.get_chat()) or "this chat"
+    participants = await event.client.get_participants(chat)
+    count = len(participants)
+    mention = f"[{a_user.first_name}](tg://user?id={a_user.id})"
+    my_mention = f"[{me.first_name}](tg://user?id={me.id})"
+    first = a_user.first_name
+    last = a_user.last_name
+    fullname = f"{first} {last}" if last else first
+    username = f"@{a_user.username}" if a_user.username else mention
+    userid = a_user.id
+    my_first = me.first_name
+    my_last = me.last_name
+    my_fullname = f"{my_first} {my_last}" if my_last else my_first
+    my_username = f"@{me.username}" if me.username else my_mention
+    for trigger in filters:
+        pattern = r"( |^|[^\w])" + re.escape(trigger.keyword) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            file_media = None
+            filter_msg = None
+            if trigger.f_mesg_id:
+                msg_o = await event.client.get_messages(
+                    entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id)
+                )
+                file_media = msg_o.media
+                filter_msg = msg_o.message
+                link_preview = True
+            elif trigger.reply:
+                filter_msg = trigger.reply
+                link_preview = False
+            await event.reply(
+                filter_msg.format(
+                    mention=mention,
+                    title=title,
+                    count=count,
+                    first=first,
+                    last=last,
+                    fullname=fullname,
+                    username=username,
+                    userid=userid,
+                    my_first=my_first,
+                    my_last=my_last,
+                    my_fullname=my_fullname,
+                    my_username=my_username,
+                    my_mention=my_mention,
+                ),
+                file=file_media,
+                link_preview=link_preview,
+            )
 
 
-@pandaub.ilhammansiz_cmd(
+@PandaBot.ilhammansiz_cmd(
     pattern="filter (.*)",
     command=("filter", plugin_category),
     info={
         "header": "To save filter for the given keyword.",
         "description": "If any user sends that filter then your bot will reply.",
-        "note": "For saving media/stickers as filters you need to set PRIVATE_GROUP_BOT_API_ID.",
+        "option": {
+            "{mention}": "To mention the user",
+            "{title}": "To get chat name in message",
+            "{count}": "To get group members",
+            "{first}": "To use user first name",
+            "{last}": "To use user last name",
+            "{fullname}": "To use user full name",
+            "{userid}": "To use userid",
+            "{username}": "To use user username",
+            "{my_first}": "To use my first name",
+            "{my_fullname}": "To use my full name",
+            "{my_last}": "To use my last name",
+            "{my_mention}": "To mention myself",
+            "{my_username}": "To use my username.",
+        },
         "usage": "{tr}filter <keyword>",
     },
 )
-async def add_new_filter(new_handler):
+async def add_new_filter(event):
     "To save the filter"
-    keyword = new_handler.pattern_match.group(1)
-    string = new_handler.text.partition(keyword)[2]
-    msg = await new_handler.get_reply_message()
+    keyword = event.pattern_match.group(1)
+    string = event.text.partition(keyword)[2]
+    msg = await event.get_reply_message()
     msg_id = None
     if msg and msg.media and not string:
         if BOTLOG:
-            await new_handler.client.send_message(
+            await event.client.send_message(
                 BOTLOG_CHATID,
                 f"#FILTER\
-            \nCHAT ID: {new_handler.chat_id}\
+            \nCHAT ID: {event.chat_id}\
             \nTRIGGER: {keyword}\
             \n\nThe following message is saved as the filter's reply data for the chat, please do NOT delete it !!",
             )
-            msg_o = await new_handler.client.forward_messages(
+            msg_o = await event.client.forward_messages(
                 entity=BOTLOG_CHATID,
                 messages=msg,
-                from_peer=new_handler.chat_id,
+                from_peer=event.chat_id,
                 silent=True,
             )
             msg_id = msg_o.id
         else:
             await edit_or_reply(
-                new_handler,
+                event,
                 "__Saving media as reply to the filter requires the__ `PRIVATE_GROUP_BOT_API_ID` __to be set.__",
             )
             return
-    elif new_handler.reply_to_msg_id and not string:
-        rep_msg = await new_handler.get_reply_message()
-        string = rep_msg.text
+    elif msg and msg.text and not string:
+        string = msg.text
+    elif not string:
+        return await edit_or_reply(event, "__What should i do ?__")
     success = "`Filter` **{}** `{} successfully`"
-    if add_filter(str(new_handler.chat_id), keyword, string, msg_id) is True:
-        return await edit_or_reply(new_handler, success.format(keyword, "added"))
-    remove_filter(str(new_handler.chat_id), keyword)
-    if add_filter(str(new_handler.chat_id), keyword, string, msg_id) is True:
-        return await edit_or_reply(new_handler, success.format(keyword, "Updated"))
-    await edit_or_reply(new_handler, f"Error while setting filter for {keyword}")
+    if add_filter(str(event.chat_id), keyword, string, msg_id) is True:
+        return await edit_or_reply(event, success.format(keyword, "added"))
+    remove_filter(str(event.chat_id), keyword)
+    if add_filter(str(event.chat_id), keyword, string, msg_id) is True:
+        return await edit_or_reply(event, success.format(keyword, "Updated"))
+    await edit_or_reply(event, f"Error while setting filter for {keyword}")
 
 
-@pandaub.ilhammansiz_cmd(
+@PandaBot.ilhammansiz_cmd(
     pattern="filters$",
     command=("filters", plugin_category),
     info={
@@ -115,24 +168,24 @@ async def on_snip_list(event):
     )
 
 
-@pandaub.ilhammansiz_cmd(
-    pattern="stop (.*)",
+@PandaBot.ilhammansiz_cmd(
+    pattern="stop ([\s\S]*)",
     command=("stop", plugin_category),
     info={
         "header": "To delete that filter . so if user send that keyword bot will not reply",
         "usage": "{tr}stop <keyword>",
     },
 )
-async def remove_a_filter(r_handler):
+async def remove_a_filter(event):
     "Stops the specified keyword."
-    filt = r_handler.pattern_match.group(1)
-    if not remove_filter(r_handler.chat_id, filt):
-        await r_handler.edit("Filter` {} `doesn't exist.".format(filt))
+    filt = event.pattern_match.group(1)
+    if not remove_filter(event.chat_id, filt):
+        await event.edit("Filter` {} `doesn't exist.".format(filt))
     else:
-        await r_handler.edit("Filter `{} `was deleted successfully".format(filt))
+        await event.edit("Filter `{} `was deleted successfully".format(filt))
 
 
-@pandaub.ilhammansiz_cmd(
+@PandaBot.ilhammansiz_cmd(
     pattern="rmfilters$",
     command=("rmfilters", plugin_category),
     info={
@@ -145,6 +198,6 @@ async def on_all_snip_delete(event):
     filters = get_filters(event.chat_id)
     if filters:
         remove_all_filters(event.chat_id)
-        await edit_or_reply(event, f"filters in current chat deleted successfully")
+        await edit_or_reply(event, "filters in current chat deleted successfully")
     else:
-        await edit_or_reply(event, f"There are no filters in this group")
+        await edit_or_reply(event, "There are no filters in this group")
