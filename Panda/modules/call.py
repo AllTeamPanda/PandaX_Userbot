f9db66f5252c483b4584e73cde10c64032e88cb2
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-04 01:27:13 +0700

    Update call.py

--- Panda/modules/call.py
+++ Panda/modules/call.py
@@ -6,7 +6,7 @@ from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 from telethon.tl.types import ChatAdminRights
 
 from Panda.events import register
-from Panda import PandaBot
+
 NO_ADMIN = "`Sorry you are not admin :)`"
 
 plugin_category = "modules"
@@ -21,15 +21,7 @@ def user_list(l, n):
     for i in range(0, len(l), n):
         yield l[i : i + n]
 
-@PandaBot.ilhammansiz_cmd(
-    command=("startvc", plugin_category),
-    info={
-        "header": "Play the voice chat.",
-        "description": "Memulai Voice Chat grup",
-        "usage": ".startvc",
-        "examples": [".startvc"],
-    },
-)
+
 @register(outgoing=True, pattern=r"^\.startvc$", groups_only=True)
 async def _(e):
     chat = await e.get_chat()
@@ -45,15 +37,7 @@ async def _(e):
     except Exception as ex:
         await e.edit(f"`{str(ex)}`")
 
-@PandaBot.ilhammansiz_cmd(
-    command=("stopvc", plugin_category),
-    info={
-        "header": "End the voice chat.",
-        "description": "Mengakhiri Voice Chat grup",
-        "usage": ".stopvc",
-        "examples": [".stopvc"],
-    },
-)
+
 @register(outgoing=True, pattern=r"^\.stopvc$", groups_only=True)
 async def _(e):
     chat = await e.get_chat()
@@ -68,15 +52,7 @@ async def _(e):
     except Exception as ex:
         await e.edit(f"`{str(ex)}`")
 
-@PandaBot.ilhammansiz_cmd(
-    command=("vcinvite", plugin_category),
-    info={
-        "header": "Menginvite the voice chat.",
-        "description": "Menginvite Voice Chat grup",
-        "usage": ".vcinvite",
-        "examples": [".vcinvite"],
-    },
-)
+
 @register(outgoing=True, pattern=r"^\.vcinvite", groups_only=True)
 async def _(e):
     await e.edit("`Inviting Members to Voice Chat...`")
