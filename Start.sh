15a18f3cea61a505102a8003cf437fd173fcaaec
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-07 10:56:00 +0700

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,99 +1,41 @@
-## PandaX_Userbot
+#!/bin/bash
 
-__ziplink () {
+
+_repolink () {
     local regex
     regex='(https?)://github.com/.+/.+'
-    if [[ $PANDA_USERBOT_REPO == "BUKAN_USERBOT" ]]
+    if [[ $USERBOT_REPO == "PANDA_USERBOT" ]]
     then
-        echo "aHR0cHM6Ly9naXRodWIuY29tL0FmdGFoQmFnYXMvTmV3X1VzZXJib3QvYXJjaGl2ZS9QYW5kYVVzZXJib3Quemlw" | base64 -d
-    elif [[ $PANDA_USERBOT_REPO == "PANDA_USERBOT" ]]
+        echo "https://github.com/ilhammansiz/PandaX_Userbot/archive/PandaUserbot.zip"
+    elif [[ $USERBOT_REPO =~ $regex ]]
     then
-        echo "aHR0cHM6Ly9naXRodWIuY29tL2lsaGFtbWFuc2l6L1BhbmRhWF9Vc2VyYm90L2FyY2hpdmUvUGFuZGFVc2VyYm90LnppcA==" | base64 -d
-    elif [[ $PANDA_USERBOT_REPO =~ $regex ]]
-    then
-        if [[ $PANDA_USERBOT_REPO_BRANCH ]]
+        if [[ $USERBOT_REPO_BRANCH ]]
         then
-            echo "${PANDA_USERBOT_REPO}/archive/${PANDA_USERBOT_REPO_BRANCH}.zip"
+            echo "${USERBOT_REPO}/archive/${USERBOT_REPO_BRANCH}.zip"
         else
-            echo "${PANDA_USERBOT_REPO}/archive/PandaUserbot.zip"
+            echo "${USERBOT_REPO}/archive/PandaUserbot.zip"
         fi
     else
-        echo "aHR0cHM6Ly9naXRodWIuY29tL0FmdGFoQmFnYXMvTmV3X1VzZXJib3QvYXJjaGl2ZS9QYW5kYVVzZXJib3Quemlw" | base64 -d
+        echo "https://github.com/ilhammansiz/PandaX_Userbot/archive/PandaUserbot.zip"
     fi
 }
 
-__repolink () {
-    local regex
-    local rlink
-    regex='(https?)://github.com/.+/.+'
-    if [[ $UPSTREAM_REPO == "BUKAN_USERBOT" ]]
-    then
-        rlink=`echo "aHR0cHM6Ly9naXRodWIuY29tL0FmdGFoQmFnYXMvTmV3X1VzZXJib3Q=" | base64 -d`
-    elif [[ $UPSTREAM_REPO == "PANDA_USERBOT" ]]
-    then
-        rlink=`echo "aHR0cHM6Ly9naXRodWIuY29tL2lsaGFtbWFuc2l6L1BhbmRhWF9Vc2VyYm90" | base64 -d`
-    elif [[ $UPSTREAM_REPO =~ $regex ]]
-    then
-        rlink=`echo "${UPSTREAM_REPO}"`
-    else
-        rlink=`echo "aHR0cHM6Ly9naXRodWIuY29tL0FmdGFoQmFnYXMvTmV3X1VzZXJib3Q=" | base64 -d`
-    fi
-    echo "$rlink"
-}
 
-
-
-
-_install_python_version() {
-    python3${pVer%.*} -c "$1"
-}
-
-_install_deploy_git() {
-    $(_install_python_version 'from git import Repo
-import sys
-OFFICIAL_UPSTREAM_REPO = "https://github.com/PandaUserbot/Dev"
-ACTIVE_BRANCH_NAME = "main"
-repo = Repo.init()
-origin = repo.create_remote("temponame", OFFICIAL_UPSTREAM_REPO)
-origin.fetch()
-repo.create_head(ACTIVE_BRANCH_NAME, origin.refs[ACTIVE_BRANCH_NAME])
-repo.heads[ACTIVE_BRANCH_NAME].checkout(True) ')
-}
-
-_start_install_git() {
-    local repolink=$(__repolink)
-    $(_install_python_version 'from git import Repo
-import sys
-OFFICIAL_UPSTREAM_REPO="'$repolink'"
-ACTIVE_BRANCH_NAME = "'$UPSTREAM_REPO_BRANCH'" or "PandaUserbot"
-repo = Repo.init()
-origin = repo.create_remote("temponame", OFFICIAL_UPSTREAM_REPO)
-origin.fetch()
-repo.create_head(ACTIVE_BRANCH_NAME, origin.refs[ACTIVE_BRANCH_NAME])
-repo.heads[ACTIVE_BRANCH_NAME].checkout(True) ')
-}
-
-
-_install_pandauserbot () {
+_install_bot () {
     local zippath
-    zippath="pandauserbot.zip"
+    zippath="USERBOT.zip"
     echo "  Downloading source code ..."
-    wget -q $(__ziplink) -O "$zippath"
+    wget -q $(_repolink) -O "$zippath"
     echo "  Unpacking Data ..."
-    PANDA_USERBOTPATH=$(zipinfo -1 "$zippath" | grep -v "/.");
+    USERBOTPATH=$(zipinfo -1 "$zippath" | grep -v "/.");
     unzip -qq "$zippath"
     echo "Done"
     echo "  Cleaning ..."
     rm -rf "$zippath"
-    _install_deploy_git
-    cd $PANDA_USERBOTPATH
-    _start_install_git
-    echo "    Starting PandaUserBot    "
-    echo "
-           ꧁༺ Panda Userbot ༻꧂
-            Menginstal........
-    "
+    sleep 5
+    cd $USERBOTPATH
+    echo " 𝐒𝐭𝐚𝐫𝐭•𝐔𝐬𝐞𝐫𝐛𝐨𝐭-  "
     python3 -m Panda
 }
 
-_install_pandauserbot
+_install_bot
