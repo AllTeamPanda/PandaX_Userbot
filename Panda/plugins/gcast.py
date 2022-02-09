7bd373e76ed5c58120e8115fedfe35f54809a4e3
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-09 09:16:41 +0700

    Update gcast.py

--- Panda/plugins/gcast.py
+++ Panda/plugins/gcast.py
@@ -1,9 +1,15 @@
-from Panda import bot, pandaub
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+# Recode by Ilham Mansiez
 
+
+from Panda import bot, PandaBot
+import Panda
 plugin_category = "plugins"
 
 
-@pandaub.ilhammansiz_cmd(
+
+@PandaBot.ilhammansiz_cmd(
     pattern="gcast(?: |$)(.*)",
     command=("gcast", plugin_category),
     info={
@@ -13,21 +19,70 @@ plugin_category = "plugins"
 )
 async def gcast(event):
     xx = event.pattern_match.group(1)
-    if not xx:
-        return await event.edit("`Berikan Saya Text Untuk Di Broadcast`")
-    tt = event.text
-    msg = tt[6:]
-    kk = await event.edit("`• 📢 Global Broadcast Di Prosess Cok...`")
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        return await edit_delete(event, "**Berikan Saya Text Untuk Di Broadcast**")
+    kk = await edit_or_reply(event, "`• 📢 Global Broadcast Di Prosess Cok...`")
     er = 0
     done = 0
-    async for x in bot.iter_dialogs():
+    async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
-            try:
-                done += 1
-                await bot.send_message(chat, msg)
-            except BaseException:
-                er += 1
+            if chat not in Panda.Gblacklist:
+                try:
+                    await event.client.send_message(chat, msg)
+                    await asyncio.sleep(0.1)
+                    done += 1
+                except FloodWaitError as anj:
+                    await asyncio.sleep(int(anj.seconds))
+                    await event.client.send_message(chat, msg)
+                    done += 1
+                except BaseException:
+                    er += 1
     await kk.edit(
         f"**╭✠╼━━━━━━❖━━━━━━━✠╮** Broadcast Terkirim Ke =** `{done}` **Grup, Broadcast Gagal Terkirim =** `{er}`**Grup**╰✠╼━━━━━━❖━━━━━━━✠╯**"
+    )
+
+
+
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="gucast(?: |$)(.*)",
+    command=("gucast", plugin_category),
+    info={
+        "header": "Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)",
+        "usage": "{tr}gucast text/media",
+    },
+)
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
+    kk = await edit_or_reply(event, "`Globally Broadcasting processing...`")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            if chat not in Panda.DEVLIST:
+                try:
+                    await event.client.send_message(chat, msg)
+                    await asyncio.sleep(0.1)
+                    done += 1
+                except FloodWaitError as anj:
+                    await asyncio.sleep(int(anj.seconds))
+                    await event.client.send_message(chat, msg)
+                    done += 1
+                except BaseException:
+                    er += 1
+    await kk.edit(
+        f"**╭✠╼━━━━━━❖━━━━━━━✠╮** Broadcast Terkirim Ke =** `{done}` **chat, Gagal Mengirim Pesan Ke = `{er}`**╰✠╼━━━━━━❖━━━━━━━✠╯"
     )
