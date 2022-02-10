e0ce38bf51a48ee3aa80ad586c47453b5f2d573f
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-10 10:14:37 +0700

    Update __init__.py

--- Panda/sql_helper/__init__.py
+++ Panda/sql_helper/__init__.py
@@ -5,14 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
-from ..Config import Config
+from ..Var import Var
 from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI)
+    engine = create_engine(Var.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
