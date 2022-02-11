99d60a983f4de6b2f543a9957289fe90babd60b8
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-12 01:39:36 +0700

    Update sqldb.py

--- Panda/sql_helper/sqldb.py
+++ Panda/sql_helper/sqldb.py
@@ -58,9 +58,11 @@ def deldb(variable):
         SESSION.commit()
 
 
-
+import os
+import sys
 from Panda.Var import Var
 from Panda.core.logger import logging
+from redis import Redis
 
 LOGS = logging.getLogger("PandaUserbot")
 
@@ -152,7 +154,103 @@ class MongoDB:
         return True
 
 
+class RedisConnection:
+    def __init__(
+        self,
+        host,
+        port,
+        password,
+        platform=None,
+        logger=LOGS,
+        *args,
+        **kwargs,
+    ):
+        if host and ":" in host:
+            spli_ = host.split(":")
+            host = spli_[0]
+            port = int(spli_[-1])
+            if host.startswith("http"):
+                logger.error("Your REDIS_URI should not start with http !")
+                import sys
+
+                sys.exit()
+        elif not host or not port:
+            logger.error("Port Number not found")
+            import sys
+
+            sys.exit()
+        kwargs["host"] = host
+        kwargs["password"] = password
+        kwargs["port"] = port
+
+        if platform.lower() == "qovery" and not host:
+            var, hash_, host, password = "", "", "", ""
+            for vars_ in os.environ:
+                if vars_.startswith("QOVERY_REDIS_") and vars.endswith("_HOST"):
+                    var = vars_
+            if var:
+                hash_ = var.split("_", maxsplit=2)[1].split("_")[0]
+            if hash:
+                kwargs["host"] = os.environ(f"QOVERY_REDIS_{hash_}_HOST")
+                kwargs["port"] = os.environ(f"QOVERY_REDIS_{hash_}_PORT")
+                kwargs["password"] = os.environ(f"QOVERY_REDIS_{hash_}_PASSWORD")
+        self.db = Redis(**kwargs)
+        self.set = self.db.set
+        self.get = self.db.get
+        self.keys = self.db.keys
+        self.ping = self.db.ping
+        self.delete = self.db.delete
+        self.re_cache()
+
+    # dict is faster than Redis
+    def re_cache(self):
+        self._cache = {}
+        for keys in self.keys():
+            self._cache.update({keys: self.get_key(keys)})
+
+    @property
+    def name(self):
+        return "Redis"
+
+    @property
+    def usage(self):
+        return sum(self.db.memory_usage(x) for x in self.keys())
+
+    def setdb(self, key, value):
+        value = str(value)
+        try:
+            value = eval(value)
+        except BaseException:
+            pass
+        self._cache.update({key: value})
+        return self.set(str(key), str(value))
+
+    def getdb(self, key):
+        if key in self._cache:
+            return self._cache[key]
+        _ = get_data(self, key)
+        self._cache.update({key: _})
+        return _
+
+    def deldb(self, key):
+        if key in self._cache:
+            del self._cache[key]
+        return bool(self.delete(str(key)))
+
+
+
+
 def pyDB():
     if MongoClient and Var.MONGO_URI:
         return MongoDB(Var.MONGO_URI)
+    from . import HOSTED_ON
 
+    return RedisConnection(
+        host=Var.REDIS_URI or Var.REDISHOST,
+        password=Var.REDIS_PASSWORD or Var.REDISPASSWORD,
+        port=Var.REDISPORT,
+        platform=HOSTED_ON,
+        decode_responses=True,
+        socket_timeout=5,
+        retry_on_timeout=True,
+    )
