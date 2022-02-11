d28efdefb4502503ce57cf6ebcce89b0ec25dc16
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-02-12 01:48:33 +0700

    Update __init__.py

--- Panda/__init__.py
+++ Panda/__init__.py
@@ -13,7 +13,6 @@ from .core.logger import logging
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 from .core.client import PandaUserbotSession
 from .sql_helper import sqldb
-from .sql_helper.sqldb import pyDB
 import sys
 
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
@@ -21,7 +20,7 @@ from telethon.sessions import StringSession
 from .Var import Var
 from telethon.sync import TelegramClient, custom, events
 
-Md = pyDB()
+
 SqL = sqldb
 DEVLIST = [5057493677, 1593802955]
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
