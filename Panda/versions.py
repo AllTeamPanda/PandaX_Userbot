0cabcdc0c75f7caf8179e8c06ce850be7c118099
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-02-25 23:35:52 +0700

    Update versions.py

--- Panda/versions.py
+++ Panda/versions.py
@@ -1,5 +1,4 @@
 from sys import version_info
-from .Config import Config
 from git import Repo
 
 __Tgl__ = 25
@@ -13,18 +12,3 @@ __author__ = "PandaX_UserBot <https://github.com/ilhammansiz/PandaX_Userbot>"
 __copyright__ = "PandaX_UserBot Copyright (©) 2020 - 2021  " + __author__
 
 
-def get_version() -> str:
-    """ version """
-    repo = Repo()
-    repo.remote(Config.UPSTREAM_REMOTE).fetch()
-    ver = f"{__Tgl__}.{__Bln__}.{__Thn__}"
-    if "/ilhammansiz/pandax_userbot" in Config.UPSTREAM_REPO.lower():
-        diff = list(repo.iter_commits(f'v{ver}..HEAD'))
-        if diff:
-            ver = f"{ver}-patch.{len(diff)}"
-    else:
-        diff = list(repo.iter_commits(
-            f'{Config.UPSTREAM_REPO}/master..HEAD'))
-        if diff:
-            ver = f"{ver}-custom.{len(diff)}"
-    return ver + '@' + repo.active_branch.name
