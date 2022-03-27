25358e4d4f5afe4b49c3ae70bc5301afd9eddeb3
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 16:52:14 +0700

    Update evaluators.py

--- Panda/plugins/evaluators.py
+++ Panda/plugins/evaluators.py
@@ -10,7 +10,7 @@ from . import *
 plugin_category = "plugins"
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="exec(?: |$|\n)(.*)",
     command=("exec", plugin_category),
     info={
@@ -50,7 +50,7 @@ async def _(event):
         )
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="eval(?: |$|\n)(.*)",
     command=("eval", plugin_category),
     info={
