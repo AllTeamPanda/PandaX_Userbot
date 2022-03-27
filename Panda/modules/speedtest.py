9ee584a4d1818cfcb14af4ebf65709d3404b5df8
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 11:55:08 +0700

    Update speedtest.py

--- Panda/modules/speedtest.py
+++ Panda/modules/speedtest.py
@@ -10,7 +10,7 @@ from Panda import PandaBot
 
 from ..core.managers import edit_or_reply
 from ..helpers.utils import reply_id
-
+from . import ilhammansiz_cmd
 plugin_category = "modules"
 
 
@@ -24,7 +24,7 @@ def convert_from_bytes(size):
     return f"{round(size, 2)} {units[n]}"
 
 
-@PandaBot.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="speedtest(?:\s|$)([\s\S]*)",
     command=("speedtest", plugin_category),
     info={
