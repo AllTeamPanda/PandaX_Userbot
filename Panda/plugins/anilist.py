1efd42620df0c8eba034f2e02337875ad6277f4c
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 16:17:04 +0700

    Update anilist.py

--- Panda/plugins/anilist.py
+++ Panda/plugins/anilist.py
@@ -23,13 +23,13 @@ from ..helpers.functions import (
     replace_text,
 )
 from ..helpers.utils import reply_id
-
+from . import ilhammansiz_cmd
 jikan = Jikan()
 url = "https://graphql.anilist.co"
 plugin_category = "plugins"
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="airing (.*)",
     command=("airing", plugin_category),
     info={
@@ -55,7 +55,7 @@ async def anilist(event):
     await edit_or_reply(event, ms_g)
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="anime(?: |$)(.*)",
     command=("anime", plugin_category),
     info={
@@ -78,7 +78,7 @@ async def anilist(event):
     await event.edit(msg, link_preview=True)
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="manga(?: |$)(.*)",
     command=("manga", plugin_category),
     info={
@@ -110,7 +110,7 @@ async def get_manga(event):
     )
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="sanime(?: |$)(.*)",
     command=("sanime", plugin_category),
     info={
@@ -156,7 +156,7 @@ async def get_manga(event):
         )
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="char(?: |$)(.*)",
     command=("char", plugin_category),
     info={
@@ -213,7 +213,7 @@ async def character(event):
     )
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="a(kaizoku|kayo)(?: |$)(.*)",
     command=("akaizoku", plugin_category),
     info={
@@ -270,7 +270,7 @@ async def anime_doqnload(event):
     await event.edit(result, parse_mode="html")
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="upcoming$",
     command=("upcoming", plugin_category),
     info={
@@ -292,7 +292,7 @@ async def upcoming(event):
     await edit_or_reply(event, rep, parse_mode="html")
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="w(hat)?anime$",
     command=("whatanime", plugin_category),
     info={
