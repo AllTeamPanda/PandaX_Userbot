891c61a718a1251036d1c1b66141805a3f8da89b
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 15:35:09 +0700

    Update transferchannel.py

--- Panda/plugins/transferchannel.py
+++ Panda/plugins/transferchannel.py
@@ -6,13 +6,13 @@ import telethon.password as pwd_mod
 from telethon.tl import functions
 
 from Panda import pandaub
-
+from . import ilhammansiz_cmd
 from ..Config import Config
 
 plugin_category = "plugins"
 
 
-@pandaub.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="otransfer (.*)",
     command=("otransfer", plugin_category),
     info={
