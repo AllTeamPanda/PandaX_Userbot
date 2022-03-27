0c919a67f4cc68261f9abb149842dc3ac74891cf
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 11:59:21 +0700

    Update trools.py

--- Panda/modules/trools.py
+++ Panda/modules/trools.py
@@ -9,13 +9,13 @@ from Panda import PandaBot
 
 from ..core.managers import edit_or_reply
 from ..helpers.utils import _pandatools, reply_id
-from . import convert_toimage, deEmojify, phcomment, threats, trap, trash
+from . import convert_toimage, deEmojify, phcomment, threats, trap, trash, ilhammansiz_cmd
 _cattools = _pandatools
 
 plugin_category = "modules"
 
 
-@PandaBot.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="trash$",
     command=("trash", plugin_category),
     info={
@@ -55,7 +55,7 @@ async def catbot(event):
     await event.client.send_file(event.chat_id, cat, reply_to=catid)
 
 
-@PandaBot.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="threats$",
     command=("threats", plugin_category),
     info={
@@ -94,7 +94,7 @@ async def catbot(event):
     await event.client.send_file(event.chat_id, cat, reply_to=catid)
 
 
-@PandaBot.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="trap(?:\s|$)([\s\S]*)",
     command=("trap", plugin_category),
     info={
@@ -143,7 +143,7 @@ async def catbot(event):
     await event.client.send_file(event.chat_id, cat, reply_to=catid)
 
 
-@PandaBot.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="phub(?:\s|$)([\s\S]*)",
     command=("phub", plugin_category),
     info={
