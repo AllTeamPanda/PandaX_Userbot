44110f10125a4ba07f6af4e959c46688e98ae301
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 11:42:34 +0700

    Update filext.py

--- Panda/modules/filext.py
+++ Panda/modules/filext.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 from .. import PandaBot
 
 from ..core.managers import edit_or_reply
-
+from . import ilhammansiz_cmd
 plugin_category = "modules"
 
 
-@PandaBot.ilhammansiz_cmd(
+@ilhammansiz_cmd(
     pattern="filext(?:\s|$)([\s\S]*)",
     command=("filext", plugin_category),
     info={
