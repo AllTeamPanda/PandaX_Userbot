6907ba576b98e560aa0344d8dddd5cdc0d601aed
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-31 09:40:52 +0700

    Update cmd_user.py

--- Panda/core/cmd_user.py
+++ Panda/core/cmd_user.py
@@ -42,7 +42,7 @@ LOGS = logging.getLogger(__name__)
 def dual_duall():
     try:
         if SqL.getdb("DUAL_HNDLR") is not None:
-            duall = SqL.setdb("DUAL_HNDLR", "/") or "/"
+            duall = SqL.setdb("DUAL_HNDLR", "/") or "®"
             return duall
         else:
             duall = SqL.setdb("DUAL_HNDLR", "/") or "/"
