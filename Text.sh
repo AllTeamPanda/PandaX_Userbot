6a403410ab64daca132905128d49e4a0cad7b3a4
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-01 01:02:07 +0700

    Create Text.sh

---
+++ Text.sh
@@ -0,0 +1,18 @@
+terakhir () {
+    echo "
+    
+=+---------------------------------------------------------+=
+꧁༺ Panda Userbot ༻꧂
+Copyright (C) 2021 PandaX_Userbot
+Powered By @diemmmmmmmmmm with Telegram
+=+---------------------------------------------------------+=
+    "
+}
+
+_start_all () {
+    pip3 install –upgrade pip
+    pip3 install --no-cache-dir -r requirements.txt
+    terakhir
+}
+
+_start_all
