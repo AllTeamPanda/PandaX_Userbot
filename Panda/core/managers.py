4cfaf22090a637b01b9576885508636c3172f0dc
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-03 21:48:50 +0700

    Update managers.py

--- Panda/core/managers.py
+++ Panda/core/managers.py
@@ -27,7 +27,7 @@ async def edit_or_reply(
     reply_to = await event.get_reply_message()
     if len(text) < 4096:
         parse_mode = parse_mode or "md"
-        if event.sender_id == DEVLIST in sudo_users:
+        if event.sender_id in sudo_users:
             if reply_to:
                 return await reply_to.reply(
                     text, parse_mode=parse_mode, link_preview=link_preview
@@ -58,7 +58,7 @@ async def edit_or_reply(
                 "https://del.dog/documents", data=text.encode("UTF-8")
             ).json()
             text = linktext + f" [here](https://del.dog/{kresult['key']})"
-        if event.sender_id == DEVLIST in sudo_users:
+        if event.sender_id in sudo_users:
             if reply_to:
                 return await reply_to.reply(text, link_preview=link_preview)
             return await event.reply(text, link_preview=link_preview)
@@ -72,7 +72,7 @@ async def edit_or_reply(
         await reply_to.reply(caption, file=file_name)
         await event.delete()
         return os.remove(file_name)
-    if event.sender_id == DEVLIST in sudo_users:
+    if event.sender_id in sudo_users:
         await event.reply(caption, file=file_name)
         await event.delete()
         return os.remove(file_name)
@@ -86,7 +86,7 @@ async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None
     parse_mode = parse_mode or "md"
     link_preview = link_preview or False
     time = time or 5
-    if event.sender_id == DEVLIST in sudo_users:
+    if event.sender_id in sudo_users:
         reply_to = await event.get_reply_message()
         pandaevent = (
             await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
