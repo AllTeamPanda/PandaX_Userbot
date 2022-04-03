c5a97a765117c5864d58e1ea6a38d5bf04864580
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-03 14:41:35 +0700

    Update and rename powertools to powertools.py

--- Panda/modules/powertools.py
+++ Panda/modules/powertools.py
@@ -24,40 +24,6 @@ plugin_category = "modules"
 catub = PandaBot
 HEROKU_APP = HEROKU_APP_NAME
 
-@ilhammansiz_cmd(
-    pattern="restart$",
-    command=("restart", plugin_category),
-    info={
-        "header": "Restarts the bot !!",
-        "usage": "{tr}restart",
-    },
-    disable_errors=True,
-)
-async def _(event):
-    "Restarts the bot !!"
-    if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted")
-    sandy = await edit_or_reply(
-        event,
-        "Restarted. `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting",
-    )
-    try:
-        ulist = get_collectionlist_items()
-        for i in ulist:
-            if i == "restart_update":
-                del_keyword_collectionlist("restart_update")
-    except Exception as e:
-        LOGS.error(e)
-    try:
-        add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
-    except Exception as e:
-        LOGS.error(e)
-    try:
-        await catub.disconnect()
-    except CancelledError:
-        pass
-    except Exception as e:
-        LOGS.error(e)
 
 
 @ilhammansiz_cmd(
