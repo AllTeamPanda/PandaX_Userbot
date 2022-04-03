0dc195003a295a1920d76b62e249c0db2f9ebb98
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-03 16:06:33 +0700

    Update format.py

--- Panda/helpers/utils/format.py
+++ Panda/helpers/utils/format.py
@@ -1,5 +1,7 @@
 import datetime
+import re
 
+import requests
 from bs4 import BeautifulSoup
 from markdown import markdown
 from telethon.tl.tlobject import TLObject
@@ -133,3 +135,24 @@ def yaml_format(obj, indent=0, max_str_len=256, max_byte_len=64):
         return repr(obj)
 
     return "".join(result)
+
+
+def paste_text(text):
+    asciich = ["**", "`", "__"]
+    for i in asciich:
+        text = re.sub(rf"\{i}", "", text)
+    try:
+        nekokey = (
+            requests.post("https://nekobin.com/api/documents", json={"content": text})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        link = f"https://nekobin.com/{nekokey}"
+    except Exception:
+        url = "https://del.dog/documents"
+        r = requests.post(url, data=text).json()
+        link = f"https://del.dog/{r['key']}"
+        if r["isUrl"]:
+            link = f"https://del.dog/v/{r['key']}"
+    return link
