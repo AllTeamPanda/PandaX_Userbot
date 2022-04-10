c67824a3cff54bfaa308262ad6509f3f320d24af
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-10 23:16:45 +0700

    Create termux_install.sh

---
+++ termux_install.sh
@@ -0,0 +1,19 @@
+## Running PandaUserbot
+
+terakhir () {
+    echo "
+    
+=+---------------------------------------------------------+=
+꧁༺ Panda Userbot ༻꧂
+Copyright (C) 2021 PandaX_Userbot
+Powered By @diemmmmmmmmmm with Telegram
+=+---------------------------------------------------------+=
+    "
+}
+
+_start_all () {
+    terakhir
+    python3 -m Panda
+}
+
+_start_all
