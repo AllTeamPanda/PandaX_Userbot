513b8ae47461f706bd313375e5f69b5973fcd925
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-10 23:44:33 +0700

    Update __main__.py

--- Panda/__main__.py
+++ Panda/__main__.py
@@ -11,6 +11,7 @@ from importlib import import_module
 from platform import python_version
 from telethon import version
 from . import Botver, LOGS, bot, vcbot
+from .database import ongrup, join
 from .modules import ALL_MODULES
 from pytgcalls import idle
 ##
@@ -27,14 +28,15 @@ except Exception as e:
 try:
     for module_name in ALL_MODULES:
         imported_module = import_module(f"Panda.modules.{module_name}")
-    LOGS.info(f"Python Version - {python_version()}")
-    LOGS.info(f"Telethon Version - {version.__version__}")
     LOGS.info(f"PandaUserbot Version - {Botver} [ BERHASIL DIAKTIFKAN ]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
     pass
 except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
+
+bot.loop.run_until_complete(join())
+bot.loop.run_until_complete(ongrup())
 
 idle()
 if len(sys.argv) not in (1, 3, 4):
