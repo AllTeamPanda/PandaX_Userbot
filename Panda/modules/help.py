ec0481eb6446114ca3201c204f2346200388a977
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-11 01:08:18 +0700

    Update help.py

--- Panda/modules/help.py
+++ Panda/modules/help.py
@@ -19,8 +19,7 @@ async def help(event):
         user = await event.client.get_me()
         string = ""
         for i in CMD_HELP:
-            string += "[" + "`" + str(i)
-            string += "`]\t"
+            string += f"[ `{str(i)}` ]"
         await edit_or_reply(
             event,
             f"**👤 Owner:** [{user.first_name}](tg://user?id={user.id})\n\n"
