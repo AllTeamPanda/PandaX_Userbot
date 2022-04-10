87811d07d5a421a9dbe5af35514b953b4f906c86
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-04-10 23:09:32 +0700

    Update tiny.py

--- Panda/modules/tiny.py
+++ Panda/modules/tiny.py
@@ -19,7 +19,7 @@ async def ultiny(event):
         return
     xx = await event.edit("`Processing tiny...`")
     ik = await event.client.download_media(reply)
-    im1 = Image.open("Panda/Version/black.png")
+    im1 = Image.open("PandaVersion/Panda/PandaBlanck.jpg")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         await bash("lottie_convert.py ult.tgs json.json")
