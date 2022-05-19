648d782b255211f04637279bc9ab2f1a6a0a554e
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-05-19 09:07:35 +0700

    Create zonadewasa.py

---
+++ Panda/modules/zonadewasa.py
@@ -0,0 +1,28 @@
+import asyncio
+import os
+import urllib
+
+import requests
+
+
+from .. import *
+from ..misc import pandacute
+
+
+
+@pandacute(pattern="tetek(?: |$)(.*)")
+async def boobs(event):
+    if event.fwd_from:
+        return
+    if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(TMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
+    a = await event.reply("Menemukan beberapa payudara besar untukmu 😂")
+    await asyncio.sleep(0.5)
+    await a.edit("Ini besar banget nih 😂")
+    nsfw = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
