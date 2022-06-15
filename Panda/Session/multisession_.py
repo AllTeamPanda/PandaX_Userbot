b785c66762a92d826b3eedfb80322277c40f4870
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-06-15 22:26:25 +0700

    Create multisession_.py

---
+++ Panda/Session/multisession_.py
@@ -0,0 +1,26 @@
+
+from .. import LOGS
+from .client import bot, vcbot
+from .pyroclient import pyrobot, pyrovcbot
+import sys
+
+from ..file import Database
+
+def TelethonPyro():
+    if Database.SESSION:
+        try:
+            bot.start()
+            vcbot.start()
+            user = bot.get_me()
+        except Exception as e:
+            LOGS.info(str(e), exc_info=True)
+            sys.exit(1)
+     
+    if Database.PyroSESSION:
+        try:
+            pyrobot.start()
+            pyrovcbot.start()
+            user = pyrobot.get_me()
+        except Exception as e:
+            LOGS.info(str(e), exc_info=True)
+            sys.exit(1)
