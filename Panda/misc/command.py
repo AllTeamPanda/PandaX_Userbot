47dc8b6cedb4ba27fac5a6a61edd95c4d2eab706
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-06-16 09:08:44 +0700

    Update command.py

--- Panda/misc/command.py
+++ Panda/misc/command.py
@@ -27,6 +27,7 @@ from ..Osdb import Osdb
 from .. import (
     bot,
     tgbot,
+    pyrobot,
 )
 
 
@@ -305,6 +306,96 @@ def callback(**args):
     def decorator(func):
         if tgbot:
             tgbot.add_event_handler(func, events.CallbackQuery(**args))
+        return func
+
+    return decorator
+
+
+
+from pyrogram.handlers import MessageHandler, EditedMessageHandler
+
+
+def pyropandacute(
+    pattern: str = None,
+    allow_sudo: bool = True,
+    disable_edited: bool = False,
+    forword=False,
+    command: str = None,
+    **args,
+):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    args["blacklist_chats"] = True
+    black_list_chats = list(Osdb.BLACK_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    if pattern is not None:
+        global _reg
+        global sudo_reg
+        global t_reg
+        if (
+            pattern.startswith(r"\#")
+            or not pattern.startswith(r"\#")
+            and pattern.startswith(r"^")
+        ):
+            _reg = sudo_reg = t_req = re.compile(pattern)
+        else:
+            _ = "\\" + Osdb.HANDLER
+            sudo_ = "\\" + Osdb.SUDO_HANDLER
+            _reg = re.compile(_ + pattern)
+            sudo_reg = re.compile(sudo_ + pattern)
+            if command is not None:
+                cmd1 = _ + command
+                cmd2 = sudo_ + command
+            else:
+                cmd1 = (
+                    (_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+                cmd2 = (
+                    (sudo_ + pattern)
+                    .replace("$", "")
+                    .replace("\\", "")
+                    .replace("^", "")
+                )
+            try:
+                LOADED_CMDS[file_test].append(cmd1)
+            except BaseException:
+                LOADED_CMDS.update({file_test: [cmd1]})
+
+    def decorator(func):
+        if not disable_edited:
+            pyrobot.add_handler(
+                func, EditedMessageHandler(**args, outgoing=True, pattern=_reg)
+            )
+        pyrobot.add_handler(
+            func, MessageHandler(**args, outgoing=True, pattern=_reg)
+        )
+        if allow_sudo:
+            if not disable_edited:
+                pyrobot.add_handler(
+                    func,
+                    EditedMessageHandler(
+                        **args, from_users=list(Osdb.SUDO_USERS), pattern=sudo_reg
+                    ),
+                )
+            pyrobot.add_handler(
+                func,
+                MessageHandler(
+                    **args, from_users=list(Osdb.SUDO_USERS), pattern=sudo_reg
+                ),
+            )
+        try:
+            LOADED_CMDS[file_test].append(func)
+        except Exception:
+            LOADED_CMDS.update({file_test: [func]})
         return func
 
     return decorator
