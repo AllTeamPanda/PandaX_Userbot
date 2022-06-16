b6b8e5e20d7aa76c0c16095b1bea1cea4a37ec45
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-06-16 08:20:36 +0700

    Update mongodb.py

--- Panda/database/mongodb.py
+++ Panda/database/mongodb.py
@@ -6,9 +6,5 @@ from ..file import Database
 
 def mongodb():
     if Database.MONGO_DB:
-        try:
-            motor.motor_asyncio.AsyncIOMotorClient(Database.MONGO_DB)
-        except Exception as e:
-            LOGS.info(str(e), exc_info=True)
-            sys.exit(1)
-     
+        return motor.motor_asyncio.AsyncIOMotorClient(Database.MONGO_DB)
+        
