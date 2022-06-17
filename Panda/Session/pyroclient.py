ca75c91d6b8fbe29888ad5c3e1623b86293f2701
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-06-17 08:13:40 +0700

    Update pyroclient.py

--- Panda/Session/pyroclient.py
+++ Panda/Session/pyroclient.py
@@ -16,6 +16,36 @@ try:
     else:
          session_name = None
          pyrobot = session_name
+    if Database.PyroSESSION2:
+        pyrobot2 = Client(
+        Database.PyroSESSION2,
+        api_id=Database.APP_ID,
+        api_hash=Database.API_HASH,
+        sleep_threshold=180,
+    )
+    else:
+         session_name = None
+         pyrobot2 = session_name
+    if Database.PyroSESSION3:
+        pyrobot3 = Client(
+        Database.PyroSESSION3,
+        api_id=Database.APP_ID,
+        api_hash=Database.API_HASH,
+        sleep_threshold=180,
+    )
+    else:
+         session_name = None
+         pyrobot3 = session_name
+    if Database.PyroSESSION4:
+        pyrobot = Client(
+        Database.PyroSESSION4,
+        api_id=Database.APP_ID,
+        api_hash=Database.API_HASH,
+        sleep_threshold=180,
+    )
+    else:
+         session_name = None
+         pyrobot4 = session_name
     if Database.BOT_TOKEN is not None:
         pyrotgbot = Client(
            "SESSION",
