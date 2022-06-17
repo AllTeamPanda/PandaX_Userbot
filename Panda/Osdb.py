6ee4e59d189cc1d698beb822b4f4a0c5f7020464
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-06-17 08:36:07 +0700

    Update Osdb.py

--- Panda/Osdb.py
+++ Panda/Osdb.py
@@ -7,6 +7,35 @@
 from distutils.util import strtobool as sb
 import os
 
+
+import heroku3
+from dotenv import load_dotenv
+from distutils.util import strtobool
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+
+def fetch_heroku_git_url(api_key, app_name):
+    if not api_key:
+        return None
+    if not app_name:
+        return None
+    heroku = heroku3.from_key(api_key)
+    try:
+        heroku_applications = heroku.apps()
+    except:
+        return None
+    heroku_app = None
+    for app in heroku_applications:
+        if app.name == app_name:
+            heroku_app = app
+            break
+    if not heroku_app:
+        return None
+    return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
+
+
 class Osdb(object):
     BOTLOG = sb(os.environ.get("BOTLOG", "True"))
     LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "True"))
@@ -53,4 +82,30 @@ class Osdb(object):
     HANDLER = os.environ.get("HANDLER") or "."
     SUDO_HANDLER = os.environ.get("SUDO_HANDLER", r"+")
     LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-    
+    LOG_GRP = BOTLOG_CHATID
+    COMMAND_HANDLER = HANDLER
+    SUDO_USERS = SUDO_USERS
+    AFS = list(SUDO_USERS)
+    CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "🐼")
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+    FBAN_GROUP = int(os.environ.get("FBAN_GROUP", False))
+    UPSTREAM_REPO = os.environ.get(
+        "UPSTREAM_REPO", "https://github.com/ilhammansiz/PandaX_Userbot"
+    )
+    ALIVE_IMG = os.environ.get(
+        "ALIVE_IMG", "https://telegra.ph/file/336d811bce4f1d1053fbf.jpg"
+    )
+    U_BRANCH = "PandaUserbot"
+    HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
+    V_T_KEY = os.environ.get("VIRUSTOTAL_API_KEY", None)
+    TAG_LOGGER = os.environ.get("TAG_LOGGER", False)
+    PM_PSW = bool(strtobool(str(os.environ.get("PM_PSW", True))))
+    MAIN_NO_LOAD = [x for x in os.environ.get("MAIN_NO_LOAD", "").split(',')]
+    XTRA_NO_LOAD = [x for x in os.environ.get("XTRA_NO_LOAD", "").split(',')]
+    DISABLED_SUDO_CMD_S = os.environ.get("DISABLED_SUDO_CMD_S", None)
+    ENABLE_WAIFU_FOR_ALL_CHATS = bool(strtobool(str(os.environ.get("ENABLE_WAIFU_FOR_ALL_CHATS", False))))
+    CHROME_DRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH", "/usr/bin/chromedriver")
+    CHROME_BIN_PATH = os.environ.get("CHROME_BIN_PATH", "/usr/bin/google-chrome-stable")
+    TZ = os.environ.get("TZ", "Asia/Jakarta")
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+
