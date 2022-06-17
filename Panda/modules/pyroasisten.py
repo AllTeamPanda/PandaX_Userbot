8db4629737dd1e726e4e9e48e924f4ec240ea689
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-06-17 09:19:40 +0700

    Create pyroasisten.py

---
+++ Panda/modules/pyroasisten.py
@@ -0,0 +1,22 @@
+
+from .. import start_time, pyrotgbot as bot
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from .._func import get_all_pros, get_readable_time
+from .._func import (
+    _check_admin,
+    _check_owner_or_sudos,
+)
+
+@bot.on_message(filters.command(["ping"]) & filters.incoming)
+@_check_owner_or_sudos
+async def ping(client, message):
+    uptime = get_readable_time((time.time() - start_time))
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await client.send_message(
+        message.chat.id,
+        f"**┏━《 **𝗣 𝗔 𝗡 𝗗 𝗔** 》━\n**┣➠  __Ping:__** `{ms}` \n ┗➠ __Uptime:__ `{uptime}`",
+    )
