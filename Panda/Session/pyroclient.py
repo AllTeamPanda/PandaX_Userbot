38c28589f98eb781d81855643bed885bcbd562f1
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-06-21 23:39:31 +0700

    Update pyroclient.py

--- Panda/Session/pyroclient.py
+++ Panda/Session/pyroclient.py
@@ -12,40 +12,40 @@ try:
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-    )
+        )
     else:
-         session_name = None
-         pyrobot = session_name
+        session_name = None
+        pyrobot = session_name
     if Database.PyroSESSION2:
         pyrobot2 = Client(
         Database.PyroSESSION2,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-    )
+        )
     else:
-         session_name = None
-         pyrobot2 = session_name
+        session_name = None
+        pyrobot2 = session_name
     if Database.PyroSESSION3:
         pyrobot3 = Client(
         Database.PyroSESSION3,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-    )
+        )
     else:
-         session_name = None
-         pyrobot3 = session_name
+        session_name = None
+        pyrobot3 = session_name
     if Database.PyroSESSION4:
         pyrobot4 = Client(
         Database.PyroSESSION4,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-    )
+        )
     else:
-         session_name = None
-         pyrobot4 = session_name
+        session_name = None
+        pyrobot4 = session_name
     if Database.BOT_TOKEN:
         pyrotgbot = Client(
            "SESSION",
@@ -53,9 +53,9 @@ try:
            api_hash=Database.API_HASH,
            bot_token=Database.BOT_TOKEN,
            sleep_threshold=180,
-            ) 
+        ) 
     else:
-         pyrotgbot = None
+        pyrotgbot = None
 except Exception as e:
     print(f"PyroSESSION {str(e)}")
     sys.exit()
