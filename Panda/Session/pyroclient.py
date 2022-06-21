0aaf153867dfb3c05b02a8d041584fd530ec45a3
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-06-21 23:47:33 +0700

    Update pyroclient.py

--- Panda/Session/pyroclient.py
+++ Panda/Session/pyroclient.py
@@ -6,58 +6,53 @@ import sys
 loop = None
 
 try:    
-    if Database.PyroSESSION:
-        pyrobot = Client(
+
+
+
+
+
+if Database.PyroSESSION:
+    pyrobot = Client(
         Database.PyroSESSION,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-        )
-    else:
-        session_name = None
-        pyrobot = session_name
-    if Database.PyroSESSION2:
-        pyrobot2 = Client(
+    )
+if Database.PyroSESSION2:
+    pyrobot2 = Client(
         Database.PyroSESSION2,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-        )
-    else:
-        session_name = None
-        pyrobot2 = session_name
-    if Database.PyroSESSION3:
-        pyrobot3 = Client(
+    )
+else:
+    pyrobot2 = None
+if Database.PyroSESSION3:
+    pyrobot3 = Client(
         Database.PyroSESSION3,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-        )
-    else:
-        session_name = None
-        pyrobot3 = session_name
-    if Database.PyroSESSION4:
-        pyrobot4 = Client(
+    )
+else:
+    pyrobot3 = None
+if Database.PyroSESSION4:
+    pyrobot4 = Client(
         Database.PyroSESSION4,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
-        )
-    else:
-        session_name = None
-        pyrobot4 = session_name
-    if Database.BOT_TOKEN:
-        pyrotgbot = Client(
-           "SESSION",
-           api_id=Database.APP_ID,
-           api_hash=Database.API_HASH,
-           bot_token=Database.BOT_TOKEN,
-           sleep_threshold=180,
-        ) 
-    else:
-        pyrotgbot = None
-except Exception as e:
-    print(f"PyroSESSION {str(e)}")
-    sys.exit()
+    )
+else:
+    pyrobot4 = None
 
-
+if Database.BOT_TOKEN:
+    pyrotgbot = Client(
+        "MyAssistant",
+        api_id=Database.APP_ID,
+        api_hash=Database.API_HASH,
+        bot_token=Database.BOT_TOKEN,
+        sleep_threshold=180,
+    )
+else:
+    pyrotgbot = None
