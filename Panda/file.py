fb5400e7ed8e396c88c8e9e43cee9bdf352fb0d2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-06-26 14:53:44 +0700

    Update file.py

--- Panda/file.py
+++ Panda/file.py
@@ -4,6 +4,7 @@ class Database(object):
     APP_ID = int(os.environ.get("API_ID", 6))
     API_HASH = os.environ.get("API_HASH") or None
     SESSION = os.environ.get("SESSION", None)
+    SESSION2 = os.environ.get("SESSION2", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     PyroSESSION = os.environ.get("PyroSESSION", None)
