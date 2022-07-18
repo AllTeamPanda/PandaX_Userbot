d1e1c63bc979eff063f048c9d5531f9a8db1281f
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-18 23:11:16 +0700

    Update mongodb.py

--- Panda/database/mongodb.py
+++ Panda/database/mongodb.py
@@ -5,4 +5,6 @@ from ..file import Database
 def mongodb():
     if Database.MONGO_DB:
         return motor.motor_asyncio.AsyncIOMotorClient(Database.MONGO_DB)
-        
+    else:
+        Database.MONGO_DB:
+         return None
