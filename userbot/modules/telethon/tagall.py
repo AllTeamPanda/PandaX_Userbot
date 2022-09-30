3e1cfa5a49ca43bd6c074f3f234e0adba9e67a57
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-09-30 08:40:24 +0700

    Add files via upload

---
+++ userbot/modules/telethon/tagall.py
@@ -0,0 +1,134 @@
+import asyncio
+import random
+import re
+
+
+from .. import PandaBot
+
+usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
+nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
+emoji = "😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 😍 🤩 🥳 🤗 🙃 🙂 ☺️ 😊 😏 😌 😉 🤭 😶 😐 😑 😔 😋 😛 😝 😜 🤪 🤔 🤨 🧐 🙄 😒 😤 😠 🤬 ☹️ 🙁 😕 😟 🥺 😳 😬 🤐 🤫 😰 😨 😧 😦 😮 😯 😲 😱 🤯 😢 😥 😓 😞 😖 😣 😩 😫 🤤 🥱 😴 😪 🌛 🌜 🌚 🌝 🎲 🧩 ♟ 🎯 🎳 🎭💕 💞 💓 💗 💖 ❤️‍🔥 💔 🤎 🤍 🖤 ❤️ 🧡 💛 💚 💙 💜 💘 💝 🐵 🦁 🐯 🐱 🐶 🐺 🐻 🐨 🐼 🐹 🐭 🐰 🦊 🦝 🐮 🐷 🐽 🐗 🦓 🦄 🐴 🐸 🐲 🦎 🐉 🦖 🦕 🐢 🐊 🐍 🐁 🐀 🐇 🐈 🐩 🐕 🦮 🐕‍🦺 🐅 🐆 🐎 🐖 🐄 🐂 🐃 🐏 🐑 🐐 🦌 🦙 🦥 🦘 🐘 🦏 🦛 🦒 🐒 🦍 🦧 🐪 🐫 🐿️ 🦨 🦡 🦔 🦦 🦇 🐓 🐔 🐣 🐤 🐥 🐦 🦉 🦅 🦜 🕊️ 🦢 🦩 🦚 🦃 🦆 🐧 🦈 🐬 🐋 🐳 🐟 🐠 🐡 🦐 🦞 🦀 🦑 🐙 🦪 🦂 🕷️ 🦋 🐞 🐝 🦟 🦗 🐜 🐌 🐚 🕸️ 🐛 🐾 🌞 🤢 🤮 🤧 🤒 🍓 🍒 🍎 🍉 🍑 🍊 🥭 🍍 🍌 🌶 🍇 🥝 🍐 🍏 🍈 🍋 🍄 🥕 🍠 🧅 🌽 🥦 🥒 🥬 🥑 🥯 🥖 🥐 🍞 🥜 🌰 🥔 🧄 🍆 🧇 🥞 🥚 🧀 🥓 🥩 🍗 🍖 🥙 🌯 🌮 🍕 🍟 🥨 🥪 🌭 🍔 🧆 🥘 🍝 🥫 🥣 🥗 🍲 🍛 🍜 🍢 🥟 🍱 🍚 🥡 🍤 🍣 🦞 🦪 🍘 🍡 🥠 🥮 🍧 🍨 📞".split(
+    " "
+)
+
+plugin_category = "modules"
+
+class FlagContainer:
+    is_active = False
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="mention(?: |$)(.*)",
+    command=("mention", plugin_category),
+    info={
+        "header": "Untuk memention semua anggota grup",
+        "usage": [
+            "{tr}mention",
+        ],
+    },
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.delete()
+    query = event.pattern_match.group(1)
+    mentions = f"@all {query}"
+    chat = await event.get_input_chat()
+    async for x in event.client.iter_participants(chat, 100500):
+        mentions += f"[\u2063](tg://user?id={x.id} {query})"
+    await event.client.send_message(
+        chat, mentions, reply_to=event.message.reply_to_msg_id
+    )
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="emojitag(?: |$)(.*)",
+    command=("emojitag", plugin_category),
+    info={
+        "header": "Untuk Mengetag semua anggota di grup dengan random emoji berbeda.",
+        "usage": [
+            "{tr}emojitag",
+        ],
+    },
+)
+async def _(event):
+    if event.fwd_from or FlagContainer.is_active:
+        return
+    try:
+        FlagContainer.is_active = True
+        args = event.message.text.split(" ", 1)
+        text = args[1] if len(args) > 1 else None
+        chat = await event.get_input_chat()
+        await event.delete()
+        tags = list(
+            map(
+                lambda m: f"[{random.choice(emoji)}](tg://user?id={m.id})",
+                await event.client.get_participants(chat),
+            ),
+        )
+        current_pack = []
+        async for participant in event.client.iter_participants(chat):
+            if not FlagContainer.is_active:
+                break
+            current_pack.append(participant)
+            if len(current_pack) == 5:
+                tags = list(
+                    map(
+                        lambda m: f"[{random.choice(emoji)}](tg://user?id={m.id})",
+                        current_pack,
+                    ),
+                )
+                current_pack = []
+                if text:
+                    tags.append(text)
+                await event.client.send_message(event.chat_id, " ".join(tags))
+                await asyncio.sleep(2)
+    finally:
+        FlagContainer.is_active = False
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="all(?: |$)(.*)",
+    command=("all", plugin_category),
+    info={
+        "header": "Untuk Mengetag semua anggota di grup",
+        "usage": [
+            "{tr}all",
+        ],
+    },
+)
+async def _(event):
+    if event.fwd_from or FlagContainer.is_active:
+        return
+    try:
+        FlagContainer.is_active = True
+        args = event.message.text.split(" ", 1)
+        text = args[1] if len(args) > 1 else None
+        chat = await event.get_input_chat()
+        await event.delete()
+        tags = list(
+            map(
+                lambda m: f"[{m.first_name}](tg://user?id={m.id})",
+                await event.client.get_participants(chat),
+            ),
+        )
+        jumlah = []
+        async for participant in event.client.iter_participants(chat):
+            if not FlagContainer.is_active:
+                break
+            jumlah.append(participant)
+            if len(jumlah) == 5:
+                tags = list(
+                    map(
+                        lambda m: f"[{m.first_name}](tg://user?id={m.id})",
+                        jumlah,
+                    ),
+                )
+                jumlah = []
+                if text:
+                    tags.append(text)
+                await event.client.send_message(event.chat_id, " ".join(tags))
+                await asyncio.sleep(2)
+    finally:
+        FlagContainer.is_active = False
+
---
+++ userbot/modules/telethon/vctools.py
@@ -0,0 +1,79 @@
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# recode by ilham
+
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+
+
+from Panda.events import register
+
+async def get_call(event):
+    mm = await event.client(getchat(event.chat_id))
+    xx = await event.client(getvc(mm.full_chat.call, limit=1))
+    return xx.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
+@register(outgoing=True, pattern=r"^\.startvc$", groups_only=True)
+async def start_voice(c):
+    me = await c.client.get_me()
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await edit_delete(c, f"**Maaf 🙏🏻 {me.first_name} Bukan Admin**")
+        return
+    try:
+        await c.client(startvc(c.chat_id))
+        await edit_or_reply(c, "`Voice Chat Started...`")
+    except Exception as ex:
+        await edit_delete(c, f"**ERROR:** `{ex}`")
+
+
+@register(outgoing=True, pattern=r"^\.stopvc$", groups_only=True)
+async def stop_voice(c):
+    me = await c.client.get_me()
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await edit_delete(c, f"**Maaf 🙏🏻 {me.first_name} Bukan Admin**")
+        return
+    try:
+        await c.client(stopvc(await get_call(c)))
+        await edit_or_reply(c, "`Voice Chat Stopped...`")
+    except Exception as ex:
+        await edit_delete(c, f"**ERROR:** `{ex}`")
+
+
+@register(outgoing=True, pattern=r"^\.vcinvite$", groups_only=True)
+async def _(c):
+    xxnx = await edit_or_reply(c, "`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in c.client.iter_participants(c.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    botman = list(user_list(users, 6))
+    for p in botman:
+        try:
+            await c.client(invitetovc(call=await get_call(c), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await xxnx.edit(f"`{z}` **Orang Berhasil diundang ke VCG**")
