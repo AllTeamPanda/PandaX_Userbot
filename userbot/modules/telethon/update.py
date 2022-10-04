af1700ab31b5616f4621ec50d46a9d381c35503d
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-10-04 11:47:29 +0700

    Update update.py

--- userbot/modules/telethon/update.py
+++ userbot/modules/telethon/update.py
@@ -1,5 +1,5 @@
 ##
-from ... import LOGS, _version_
+from ... import LOGS, __version__
 from . import PandaBot, edit_or_reply
 import asyncio
 from base64 import b64decode
@@ -17,8 +17,8 @@ async def gen_chlog(repo, diff):
     UPSTREAM_REPO_URL = b64decode("aHR0cHM6Ly9naXRodWIuY29tL2lsaGFtbWFuc2l6L1BhbmRhWF9Vc2VyYm90").decode("utf-8")
     ac_br = repo.active_branch.name
     ch_log = tldr_log = ""
-    ch = f"<b>PandaUserbot {_version_} updates for <a href={UPSTREAM_REPO_URL}/tree/{ac_br}>[{ac_br}]</a>:</b>"
-    ch_tl = f"PandaUserbot {_version_} updates for {ac_br}:"
+    ch = f"<b>PandaUserbot {__version__} updates for <a href={UPSTREAM_REPO_URL}/tree/{ac_br}>[{ac_br}]</a>:</b>"
+    ch_tl = f"PandaUserbot {__version__} updates for {ac_br}:"
     d_form = "%d/%m/%y || %H:%M"
     for c in repo.iter_commits(diff):
         ch_log += f"\n\n💬 <b>{c.count()}</b> 🗓 <b>[{c.committed_datetime.strftime(d_form)}]</b>\n<b><a href={UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}>[{c.summary}]</a></b> 👨‍💻 <code>{c.author}</code>"
