dd9c94f733a72ad7e64ae87d09f272066ed1dfc1
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-10-05 08:25:39 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,6 +12,7 @@ from userbot import LOGS
 from .Var import Database
 from .Session.multisession_ import Pyrogram, Telethon
 
+"""
 from . import resources
 
 
@@ -26,23 +27,16 @@ def start():
         userbot.PandaBot.loop.run_until_complete(resources.join())
         LOGS.info(f"꧁༺ Panda Userbot ༻꧂\n⚙️ Version:{userbot.__version__} [TELAH DIAKTIFKAN]")
 
-
+"""
 
 if __name__ == "__main__":
     if Database.SESSION:
         Telethon()
+        """
         start()
+        """
     if Database.PyroSESSION:
         Pyrogram()
         
     
-    
-if userbot.PandaBot:
-    try:
-        if len(sys.argv) not in (1, 3, 4):
-            userbot.PandaBot.disconnect()
-        else:
-            userbot.PandaBot.run_until_disconnected()
-    except Exception as e:
-        LOGS.info(str(e), exc_info=True)
-        sys.exit(1)
+   
