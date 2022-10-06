5cc105ebc79c6c9abfc15e481dac67a30c82755e
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-10-06 10:10:07 +0700

    Update secret.py

--- userbot/modules/telethon/assistant/secret.py
+++ userbot/modules/telethon/assistant/secret.py
@@ -13,8 +13,8 @@ pandaub = PandaBot
 @tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./Panda/secrets.txt"):
-        jsondata = json.load(open("./Panda/secrets.txt"))
+    if os.path.exists("./userbot/secrets.txt"):
+        jsondata = json.load(open("./userbot/secrets.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
