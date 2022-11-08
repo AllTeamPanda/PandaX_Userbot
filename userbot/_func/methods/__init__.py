025a746a6248ff35b23680795cfe04940856656a
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-11-08 09:37:51 +0700

    Add files via upload

--- userbot/_func/methods/__init__.py
+++ userbot/_func/methods/__init__.py
@@ -1,1 +1,11 @@
+from .on_callback import OnCallbackQuery
+from .on_inline import OnInlineQuery
+from .on_message import OnMessage
 
+
+class Decorators(
+    OnMessage,
+    OnInlineQuery,
+    OnCallbackQuery,
+):
+    pass
