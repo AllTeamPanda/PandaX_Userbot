94a2ce26146dd1ff5adab7cc3d5d7991d7eb2273
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-11-08 09:11:28 +0700

    Add files via upload

--- userbot/helpers/datavars/__init__.py
+++ userbot/helpers/datavars/__init__.py
@@ -1,1 +1,7 @@
+from .botconfig import BOTDV, BotConfig
+from .otherconfig import OTHERDV, OtherConfig
+from .userconfig import USERDV, UserConfig
 
+
+class DataVars(BotConfig, OtherConfig, UserConfig):
+    DVLIST = BOTDV + OTHERDV + USERDV
