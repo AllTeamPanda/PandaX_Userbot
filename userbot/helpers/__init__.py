262404288bc8ceacf11e1e99ed5876afbe6b08db
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-11-08 09:27:57 +0700

    Update __init__.py

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -21,3 +21,13 @@ while flag:
         check += 1
         if check > 5:
             break
+
+from .containers import Containers
+from .datavars import DataVars
+from .decorators import Decorators
+from .filters import *
+from .functions import Functions
+
+
+class Helpers(Containers, Functions, Decorators, DataVars):
+    pass
