3b05892c5714fcb001ef8cf56f409569d76f1fbf
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-11-10 00:01:06 +0700

    Update __init__.py

--- userbot/modules/pyrogram/__init__.py
+++ userbot/modules/pyrogram/__init__.py
@@ -20,3 +20,23 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
     basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
+
+
+
+import os
+
+
+def __list_all_modules():
+    import glob
+    from os.path import basename, dirname, isfile
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+MODULES = sorted(__list_all_modules())
