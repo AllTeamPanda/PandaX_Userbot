fca45d109f109a0536739d11919a62c8a9927a5b
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-12-14 07:09:02 +0700

    Update pyroclient.py

--- userbot/Session/pyroclient.py
+++ userbot/Session/pyroclient.py
@@ -10,38 +10,40 @@ from .classbot import Pyrobot
 from .._database._var import Database
 
 loop = None
+from .._database import DatabaseCute
+DB = DatabaseCute()
 
 
-if Database.PyroSESSION:
+if DB.getdb("PyroSESSION") or Database.PyroSESSION:
     pyrobot = Client(
-        Database.PyroSESSION,
+        DB.getdb("PyroSESSION") or Database.PyroSESSION,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
     )
 else:
     pyrobot = None
-if Database.PyroSESSION2:
+if DB.getdb("PyroSESSION2") or Database.PyroSESSION2:
     pyrobot2 = Client(
-        Database.PyroSESSION2,
+        DB.getdb("PyroSESSION2") or Database.PyroSESSION2,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
     )
 else:
     pyrobot2 = None
-if Database.PyroSESSION3:
+if DB.getdb("PyroSESSION3") or Database.PyroSESSION3:
     pyrobot3 = Client(
-        Database.PyroSESSION3,
+        DB.getdb("PyroSESSION3") or Database.PyroSESSION3,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
     )
 else:
     pyrobot3 = None
-if Database.PyroSESSION4:
+if DB.getdb("PyroSESSION4") or Database.PyroSESSION4:
     pyrobot4 = Client(
-        Database.PyroSESSION4,
+        DB.getdb("PyroSESSION4") or Database.PyroSESSION4,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
@@ -49,13 +51,13 @@ if Database.PyroSESSION4:
 else:
     pyrobot4 = None
 
-if Database.BOT_TOKEN:
+if DB.getdb("BOT_TOKEN") or Database.BOT_TOKEN:
     pyrotgbot = Pyrobot()
 
 
 from .classpyro import SuperClient
 
-if Database.PyroSESSION:
+if DB.getdb("PyroSESSION") or Database.PyroSESSION:
     app = SuperClient()
 else:
     app = None
