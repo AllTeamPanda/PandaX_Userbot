cc1e9599de4896dd777b613a9d9a4bfbb10184a1
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-27 19:57:19 +0700

    Update url_database.py

--- userbot/_database/url_database.py
+++ userbot/_database/url_database.py
@@ -5,13 +5,14 @@
 
 # Recode by @robotrakitangakbagus, @diemmmmmmmmmm
 # Import PandaX_Userbot <https://github.com/ilhammansiz/PandaX_Userbot>
-# t.me/PandaUserbot & t.me/TeamSquadUserbotSupport
+# t.me/PandaUserbot & t.me/pandac0de
+
 import logging
 import ast
 import os
 import sys
 
-run_as_module = False
+
 
 def where_hosted():
     if os.getenv("DYNO"):
@@ -34,18 +35,30 @@ HOSTED_ON = where_hosted()
 LOGS = logging.getLogger("PandaUserbot")
 loop = None
 
-if run_as_module:
-    from ._var import Var
 
 from ._var import Var
 
-psycopg2 = Database = None
-if Var.DATABASE_URL:
+Redis = MongoClient = psycopg2 = Database = None
+if Var.REDIS_URI or Var.REDISHOST:
+    try:
+        from redis import Redis
+    except ImportError:
+        LOGS.info("Installing 'redis' for database.")
+        os.system(f"{sys.executable} -m pip install -q redis hiredis")
+        from redis import Redis
+elif Var.MONGO_URI:
+    try:
+        from pymongo import MongoClient
+    except ImportError:
+        LOGS.info("Installing 'pymongo' for database.")
+        os.system(f"{sys.executable} -m pip install -q pymongo[srv]")
+        from pymongo import MongoClient
+elif Var.DATABASE_URL:
     try:
         import psycopg2
     except ImportError:
         LOGS.info("Installing 'pyscopg2' for database.")
-        os.system("pip3 install -q psycopg2-binary")
+        os.system(f"{sys.executable} -m pip install -q psycopg2-binary")
         import psycopg2
 else:
     try:
@@ -62,7 +75,7 @@ class _BaseDatabase:
     def __init__(self, *args, **kwargs):
         self._cache = {}
 
-    def getdb(self, key):
+    def get_key(self, key):
         if key in self._cache:
             return self._cache[key]
         value = self._get_data(key)
@@ -72,7 +85,7 @@ class _BaseDatabase:
     def re_cache(self):
         self._cache.clear()
         for key in self.keys():
-            self._cache.update({key: self.getdb(key)})
+            self._cache.update({key: self.get_key(key)})
 
     def ping(self):
         return "Active"
@@ -84,7 +97,7 @@ class _BaseDatabase:
     def keys(self):
         return []
 
-    def deldb(self, key):
+    def del_key(self, key):
         if key in self._cache:
             del self._cache[key]
         self.delete(key)
@@ -100,18 +113,62 @@ class _BaseDatabase:
                 pass
         return data
 
-    def setdb(self, key, value):
+    def set_key(self, key, value):
         value = self._get_data(data=value)
         self._cache[key] = value
         return self.set(str(key), str(value))
 
     def rename(self, key1, key2):
-        _ = self.getdb(key1)
+        _ = self.get_key(key1)
         if _:
-            self.deldb(key1)
-            self.setdb(key2, _)
+            self.del_key(key1)
+            self.set_key(key2, _)
             return 0
         return 1
+
+
+class MongoDB(_BaseDatabase):
+    def __init__(self, key, dbname="pyDatabase"):
+        self.dB = MongoClient(key, serverSelectionTimeoutMS=5000)
+        self.db = self.dB[dbname]
+        super().__init__()
+
+    def __repr__(self):
+        return f"<Panda.MonGoDB\n -total_keys: {len(self.keys())}\n>"
+
+    @property
+    def name(self):
+        return "Database_Mongo"
+
+    @property
+    def usage(self):
+        return self.db.command("dbstats")["dataSize"]
+
+    def ping(self):
+        if self.dB.server_info():
+            return True
+
+    def keys(self):
+        return self.db.list_collection_names()
+
+    def set(self, key, value):
+        if key in self.keys():
+            self.db[key].replace_one({"_id": key}, {"value": str(value)})
+        else:
+            self.db[key].insert_one({"_id": key, "value": str(value)})
+        return True
+
+    def delete(self, key):
+        self.db.drop_collection(key)
+
+    def get(self, key):
+        if x := self.db[key].find_one({"_id": key}):
+            return x["value"]
+
+    def flushall(self):
+        self.dB.drop_database("pyDatabase")
+        self._cache.clear()
+        return True
 
 
 # --------------------------------------------------------------------------------------------- #
@@ -201,23 +258,96 @@ class SqlDB(_BaseDatabase):
         )
         return True
 
+class RedisDB(_BaseDatabase):
+    def __init__(
+        self,
+        host,
+        port,
+        password,
+        platform="",
+        logger=LOGS,
+        *args,
+        **kwargs,
+    ):
+        if host and ":" in host:
+            spli_ = host.split(":")
+            host = spli_[0]
+            port = int(spli_[-1])
+            if host.startswith("http"):
+                logger.error("Your REDIS_URI should not start with http !")
+                import sys
+
+                sys.exit()
+        elif not host or not port:
+            logger.error("Port Number not found")
+            import sys
+
+            sys.exit()
+        kwargs["host"] = host
+        kwargs["password"] = password
+        kwargs["port"] = port
+
+        if platform.lower() == "qovery" and not host:
+            var, hash_, host, password = "", "", "", ""
+            for vars_ in os.environ:
+                if vars_.startswith("QOVERY_REDIS_") and vars.endswith("_HOST"):
+                    var = vars_
+            if var:
+                hash_ = var.split("_", maxsplit=2)[1].split("_")[0]
+            if hash:
+                kwargs["host"] = os.environ.get(f"QOVERY_REDIS_{hash_}_HOST")
+                kwargs["port"] = os.environ.get(f"QOVERY_REDIS_{hash_}_PORT")
+                kwargs["password"] = os.environ.get(f"QOVERY_REDIS_{hash_}_PASSWORD")
+        self.db = Redis(**kwargs)
+        self.set = self.db.set
+        self.get = self.db.get
+        self.keys = self.db.keys
+        self.delete = self.db.delete
+        super().__init__()
+
+    @property
+    def name(self):
+        return "Database_Redis"
+
+    @property
+    def usage(self):
+        return sum(self.db.memory_usage(x) for x in self.keys())
+
 
 
 class LocalDB(_BaseDatabase):
     def __init__(self):
         self.db = Database("panda")
+        self.get = self.db.get
+        self.set = self.db.set
+        self.delete = self.db.delete
         super().__init__()
+
+    @property
+    def name(self):
+        return "LocalDB"
 
     def keys(self):
         return self._cache.keys()
 
     def __repr__(self):
-        return f"<DatabaseCute.LocalDB\n -total_keys: {len(self.keys())}\n>"
+        return f"<Panda.LocalDB\n -total_keys: {len(self.keys())}\n>"
 
-
-def DatabaseCute():
+def pyDatabase():
     _er = False
     try:
+        if Redis:
+            return RedisDB(
+                host=Var.REDIS_URI or Var.REDISHOST,
+                password=Var.REDIS_PASSWORD or Var.REDISPASSWORD,
+                port=Var.REDISPORT,
+                platform=HOSTED_ON,
+                decode_responses=True,
+                socket_timeout=5,
+                retry_on_timeout=True,
+            )
+        if MongoClient:
+            return MongoDB(Var.MONGO_URI)
         if psycopg2:
             return SqlDB(Var.DATABASE_URL)
     except BaseException as err:
@@ -225,8 +355,8 @@ def DatabaseCute():
         _er = True
     if not _er:
         LOGS.critical(
-            "No DB requirement fullfilled!\nPlease install sql dependencies...\nTill then using local file as database."
+            "No DB requirement fullfilled!\nPlease install redis, mongo or sql dependencies...\nTill then using local file as database."
         )
-    if HOSTED_ON == "termux":
-        return HOSTED_ON
+    if HOSTED_ON == "local":
+        return LocalDB()
     exit()
