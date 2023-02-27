4b9a2fda974e8c16aad46c8598a0ac1d88151578
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-27 20:04:21 +0700

    Update pyroclient.py

--- userbot/Session/pyroclient.py
+++ userbot/Session/pyroclient.py
@@ -10,40 +10,40 @@ from .classbot import Pyrobot
 from .._database._var import Database
 
 loop = None
-from .._database import DatabaseCute
-DB = DatabaseCute()
+from .._database import pyDatabase
+DB = pyDatabase()
 
 
-if DB.getdb("PyroSESSION") or Database.PyroSESSION:
+if DB.get_key("PyroSESSION") or Database.PyroSESSION:
     pyrobot = Client(
-        DB.getdb("PyroSESSION") or Database.PyroSESSION,
+        DB.get_key("PyroSESSION") or Database.PyroSESSION,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
     )
 else:
     pyrobot = None
-if DB.getdb("PyroSESSION2") or Database.PyroSESSION2:
+if DB.get_key("PyroSESSION2") or Database.PyroSESSION2:
     pyrobot2 = Client(
-        DB.getdb("PyroSESSION2") or Database.PyroSESSION2,
+        DB.get_key("PyroSESSION2") or Database.PyroSESSION2,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
     )
 else:
     pyrobot2 = None
-if DB.getdb("PyroSESSION3") or Database.PyroSESSION3:
+if DB.get_key("PyroSESSION3") or Database.PyroSESSION3:
     pyrobot3 = Client(
-        DB.getdb("PyroSESSION3") or Database.PyroSESSION3,
+        DB.get_key("PyroSESSION3") or Database.PyroSESSION3,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
     )
 else:
     pyrobot3 = None
-if DB.getdb("PyroSESSION4") or Database.PyroSESSION4:
+if DB.get_key("PyroSESSION4") or Database.PyroSESSION4:
     pyrobot4 = Client(
-        DB.getdb("PyroSESSION4") or Database.PyroSESSION4,
+        DB.get_key("PyroSESSION4") or Database.PyroSESSION4,
         api_id=Database.APP_ID,
         api_hash=Database.API_HASH,
         sleep_threshold=180,
@@ -51,14 +51,14 @@ if DB.getdb("PyroSESSION4") or Database.PyroSESSION4:
 else:
     pyrobot4 = None
 
-if DB.getdb("PyroSESSION") or Database.PyroSESSION and DB.getdb("BOT_TOKEN") or Database.BOT_TOKEN:
+if DB.get_key("PyroSESSION") or Database.PyroSESSION and DB.getdb("BOT_TOKEN") or Database.BOT_TOKEN:
     pyrotgbot = Pyrobot()
 else:
     pyrotgbot = None
 
 from .classpyro import SuperClient
 
-if DB.getdb("PyroSESSION") or Database.PyroSESSION:
+if DB.get_key("PyroSESSION") or Database.PyroSESSION:
     app = SuperClient()
 else:
     app = None
