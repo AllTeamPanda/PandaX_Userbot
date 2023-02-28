38a4d6d04f1fdf0149a64dd25dae1c78c34d61ad
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-01 01:26:58 +0700

    Update managers.py

--- userbot/_misc/managers.py
+++ userbot/_misc/managers.py
@@ -1,90 +1,33 @@
 import asyncio
-import os
 
-from ..helpers.utils.format import md_to_text, paste_message
-from .data import _sudousers_list
+from .._database import gvarstatus
 
 
-# https://t.me/c/1220993104/623253
-# https://docs.telethon.dev/en/latest/misc/changelog.html#breaking-changes
-async def edit_or_reply(
-    event,
-    text,
-    parse_mode=None,
-    link_preview=None,
-    file_name=None,
-    aslink=False,
-    deflink=False,
-    noformat=False,
-    linktext=None,
-    caption=None,
-):  # sourcery no-metrics
-    sudo_users = _sudousers_list()
-    link_preview = link_preview or False
-    reply_to = await event.get_reply_message()
-    if len(text) < 4096 and not deflink:
-        parse_mode = parse_mode or "md"
-        if event.sender_id in sudo_users:
-            if reply_to:
-                return await reply_to.reply(
-                    text, parse_mode=parse_mode, link_preview=link_preview
-                )
-            return await event.reply(
-                text, parse_mode=parse_mode, link_preview=link_preview
-            )
-        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
-        return event
-    if not noformat:
-        text = md_to_text(text)
-    if aslink or deflink:
-        linktext = linktext or "Message was to big so pasted to bin"
-        response = await paste_message(text, pastetype="s")
-        text = f"{linktext} [here]({response})"
-        if event.sender_id in sudo_users:
-            if reply_to:
-                return await reply_to.reply(text, link_preview=link_preview)
-            return await event.reply(text, link_preview=link_preview)
-        await event.edit(text, link_preview=link_preview)
-        return event
-    file_name = file_name or "output.txt"
-    caption = caption or None
-    with open(file_name, "w+") as output:
-        output.write(text)
-    if reply_to:
-        await reply_to.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    if event.sender_id in sudo_users:
-        await event.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    await event.client.send_file(event.chat_id, file_name, caption=caption)
-    await event.delete()
-    os.remove(file_name)
+# edit or reply
 
 
-async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
-    sudo_users = _sudousers_list()
-    parse_mode = parse_mode or "md"
-    link_preview = link_preview or False
-    time = time or 5
-    if event.sender_id in sudo_users:
-        reply_to = await event.get_reply_message()
-        catevent = (
-            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
-            if reply_to
-            else await event.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
-        )
+async def eor(event, text, **args):
+    link_preview = args.get("link_preview", False)
+    parse_mode = args.get("parse_mode", "md")
+    time = args.get("time", None)
+    if not event.out:
+        if event.is_reply:
+            event = await event.get_reply_message()
+        ok = await event.reply(text, link_preview=link_preview, parse_mode=parse_mode)
     else:
-        pandaevent = await event.edit(
-            text, link_preview=link_preview, parse_mode=parse_mode
-        )
-    await asyncio.sleep(time)
-    return await pandaevent.delete()
+        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
+    ut = gvarstatus("DEL_DELAY_TIME")
+    if time and ut != "None":
+        time = time or ut
+        await asyncio.sleep(int(time))
+        return await ok.delete()
+    return ok
 
 
+async def eod(event, text=None, **args):
+    time = args.get("time", 5)
+    return await eor(event, text, time=time)
 
-eor = edit_or_reply
-eod = edit_delete
+
+edit_or_reply = eor
+edit_delete = eod
