4c41da09e42c62b686b96c27453b1b1ca58edb63
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-28 15:30:54 +0700

    Update admin.py

--- userbot/modules/telethon/admin.py
+++ userbot/modules/telethon/admin.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+# t.me/pandac0de t.me/pandauserbot
+
 from asyncio import sleep
 from telethon import functions
 from telethon.errors import (
