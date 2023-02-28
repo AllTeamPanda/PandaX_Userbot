331b57821e81a0f5b52dc98055bdfe1b6472407b
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-28 13:47:20 +0700

    Add files via upload

---
+++ userbot/_func/methods/on_callback.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+
+from typing import Callable
+
+import pyrogram
+from pyrogram.filters import Filter
+from pyrogram.scaffold import Scaffold
+
+
+class OnCallbackQuery(Scaffold):
+    def on_callback(self=None, filters=None, group: int = 0) -> callable:
+        def decorator(func: Callable) -> Callable:
+            if isinstance(self, pyrogram.Client):
+                self.add_handler(
+                    pyrogram.handlers.CallbackQueryHandler(func, filters), group
+                )
+            elif isinstance(self, Filter) or self is None:
+                if not hasattr(func, "handlers"):
+                    func.handlers = []
+
+                func.handlers.append(
+                    (
+                        pyrogram.handlers.CallbackQueryHandler(func, self),
+                        group if filters is None else filters,
+                    )
+                )
+
+            return func
+
+        return decorator
---
+++ userbot/_func/methods/on_message.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+
+from typing import Callable
+
+import pyrogram
+from pyrogram.filters import Filter
+from pyrogram.scaffold import Scaffold
+
+
+class OnMessage(Scaffold):
+    def on_message(self=None, filters=None, group: int = 0) -> callable:
+        """Decorator for handling messages : (user defined method)."""
+
+        def decorator(func: Callable) -> Callable:
+            if isinstance(self, pyrogram.Client):
+                self.add_handler(pyrogram.handlers.MessageHandler(func, filters), group)
+            elif isinstance(self, Filter) or self is None:
+                if not hasattr(func, "handlers"):
+                    func.handlers = []
+
+                func.handlers.append(
+                    (
+                        pyrogram.handlers.MessageHandler(func, self),
+                        group if filters is None else filters,
+                    )
+                )
+
+            return func
+
+        return decorator
