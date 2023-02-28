780825ec0b0c4a4995ac98fa2417fcc4452dffde
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-28 14:43:35 +0700

    Update decorators.py

--- userbot/_misc/decorators.py
+++ userbot/_misc/decorators.py
@@ -19,7 +19,9 @@ DEVLIST = [5057493677, 1593802955]
 
 def check_owner(func):
     async def wrapper(c_q: CallbackQuery):
-        if c_q.query.user_id == Config.OWNER_ID or c_q.query.user_id in Config.SUDO_USERS:
+        if c_q.query.user_id and not (
+            c_q.query.user_id == Config.OWNER_ID or c_q.query.user_id in Config.SUDO_USERS
+            ):
             try:
                 await func(c_q)
             except FloodWaitError as e:
