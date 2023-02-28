e86d88859c060b55512bca63b3f5b4cf6209694b
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-28 13:54:28 +0700

    Add files via upload

--- userbot/_helper/datavars/__init__.py
+++ userbot/_helper/datavars/__init__.py
@@ -1,1 +1,14 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
 
+
+from .botconfig import BOTDV, BotConfig
+from .otherconfig import OTHERDV, OtherConfig
+from .userconfig import USERDV, UserConfig
+
+
+class DataVars(BotConfig, OtherConfig, UserConfig):
+    DVLIST = BOTDV + OTHERDV + USERDV
