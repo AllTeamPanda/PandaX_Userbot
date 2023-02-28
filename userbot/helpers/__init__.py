54dd9632696c89e722d90b4c6b7bab233051b3a3
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-28 13:23:06 +0700

    Add files via upload

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -1,1 +1,32 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+# t.me/pandac0de t.me/pandauserbot
+
+
+from . import fonts
+from . import memeshelper as pandamemes
+from .aiohttp_helper import AioHttp
+from .utils import *
+
+flag = True
+check = 0
+while flag:
+    try:
+        from .chatbot import *
+        from .functions import *
+        from .memeifyhelpers import *
+        from .progress import *
+        from .qhelper import process
+        from .tools import *
+        from .utils import _format, _pandatools, _pandautils
+
+        break
+    except ModuleNotFoundError as e:
+        install_pip(e.name)
+        check += 1
+        if check > 5:
+            break
 
