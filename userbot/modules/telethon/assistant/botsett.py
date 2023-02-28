edbd9e3a826fbe43e213b2b362cba5e6dc388c0c
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-28 21:06:44 +0700

    Update botsett.py

--- userbot/modules/telethon/assistant/botsett.py
+++ userbot/modules/telethon/assistant/botsett.py
@@ -1,7 +1,9 @@
-# from https://github.com/sandy1709/catuserbot
-# Recode by ILHAM MANSIEZ
-# PANDA USERBOT
-# TENTANG AKU DAN DIA
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+# t.me/pandac0de t.me/pandauserbot
 
 
 
