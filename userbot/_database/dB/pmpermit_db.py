209a9fbc544b7c255a70c7832252ae0af57e6f16
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-02 00:15:21 +0700

    Update pmpermit_db.py

--- userbot/_database/dB/pmpermit_db.py
+++ userbot/_database/dB/pmpermit_db.py
@@ -4,20 +4,20 @@ def get_approved():
     return udB.get_key("PMPERMIT") or []
 
 
-def approve_user(id):
+def approve_user(chat_id):
     ok = get_approved()
-    if id in ok:
+    if chat_id in ok:
         return True
-    ok.append(id)
+    ok.append(chat_id)
     return udB.set_key("PMPERMIT", ok)
 
 
-def disapprove_user(id):
+def disapprove_user(chat_id):
     ok = get_approved()
-    if id in ok:
-        ok.remove(id)
+    if chat_id in ok:
+        ok.remove(chat_id)
         return udB.set_key("PMPERMIT", ok)
 
 
-def is_approved(id):
-    return id in get_approved()
+def is_approved(chat_id):
+    return chat_id in get_approved()
