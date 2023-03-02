533d338c759ab367b242566ef58d2045b38ed910
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-02 11:17:18 +0700

    Update gban_mute_db.py

--- userbot/_database/dB/gban_mute_db.py
+++ userbot/_database/dB/gban_mute_db.py
@@ -13,40 +13,40 @@ def list_gbanned():
     return udB.get_key("GBAN") or {}
 
 
-def gban(user, reason):
+def gban(chat_id, reason):
     ok = list_gbanned()
-    ok.update({int(user): reason or "No Reason. "})
+    ok.update({str(chat_id): str(reason) or "No Reason. "})
     return udB.set_key("GBAN", ok)
 
 
-def ungban(user):
+def ungban(chat_id):
     ok = list_gbanned()
-    if ok.get(int(user)):
-        del ok[int(user)]
+    if ok.get(str(chat_id)):
+        del ok[str(chat_id)]
         return udB.set_key("GBAN", ok)
 
 
-def is_gbanned(user):
+def is_gbanned(chat_id):
     ok = list_gbanned()
-    if ok.get(int(user)):
-        return ok[int(user)]
+    if ok.get(str(chat_id)):
+        return ok[str(chat_id)]
 
 
-def gmute(user):
+def gmute(sender):
     ok = list_gmuted()
-    ok.append(int(user))
+    ok.append(str(sender))
     return udB.set_key("GMUTE", ok)
 
 
-def ungmute(user):
+def ungmute(sender):
     ok = list_gmuted()
-    if user in ok:
-        ok.remove(int(user))
+    if sender in ok:
+        ok.remove(str(sender))
         return udB.set_key("GMUTE", ok)
 
 
-def is_gmuted(user):
-    return int(user) in list_gmuted()
+def is_gmuted(sender_id):
+    return str(sender_id) in list_gmuted()
 
 
 def list_gmuted():
