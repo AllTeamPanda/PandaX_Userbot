69f21a079cd11c3470af312b118cd5bd57940bc1
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-03 00:10:13 +0700

    Update client.py

--- userbot/Session/client.py
+++ userbot/Session/client.py
@@ -1,9 +1,20 @@
 
+
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#for session
+
 # Panda Userbot <https://github.com/ilhammansiz/PandaX_Userbot>
 # Recode by Ilham Mansiz
 # ••••••••••••••••••••••√•••••••••••••√√√••••••••
+import base64
+import ipaddress
+import struct
+import sys
 
-from telethon.sessions import StringSession
+from telethon.errors.rpcerrorlist import AuthKeyDuplicatedError
+from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringSession
+
 from .._database._var import Var, Database
 from telethon import TelegramClient
 import os
@@ -32,11 +43,66 @@ DUAL_MODE = DB.get_key("DUAL_MODE")
 
 
 
+_PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
+
+# https://github.com/pyrogram/pyrogram/blob/master/docs/source/faq/what-are-the-ip-addresses-of-telegram-data-centers.rst
+
+DC_IPV4 = {
+    1: "149.154.175.53",
+    2: "149.154.167.51",
+    3: "149.154.175.100",
+    4: "149.154.167.91",
+    5: "91.108.56.130",
+}
+
+
+def PandaSession(session, logger=LOGS, _exit=True):
+    if session:
+        # Telethon Session
+        if session.startswith(CURRENT_VERSION):
+            if len(session.strip()) != 353:
+                logger.exception("Wrong string session. Copy paste correctly!")
+                sys.exit()
+            return StringSession(session)
+
+        # Pyrogram Session
+        elif len(session) in _PYRO_FORM.keys():
+            data_ = struct.unpack(
+                _PYRO_FORM[len(session)],
+                base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
+            )
+            if len(session) in [351, 356]:
+                auth_id = 2
+            else:
+                auth_id = 3
+            dc_id, auth_key = data_[0], data_[auth_id]
+            return StringSession(
+                CURRENT_VERSION
+                + base64.urlsafe_b64encode(
+                    struct.pack(
+                        _STRUCT_PREFORMAT.format(4),
+                        dc_id,
+                        ipaddress.ip_address(DC_IPV4[dc_id]).packed,
+                        443,
+                        auth_key,
+                    )
+                ).decode("ascii")
+            )
+        else:
+            logger.exception("Wrong string session. Copy paste correctly!")
+            if _exit:
+                sys.exit()
+    logger.exception("No String Session found. Quitting...")
+    if _exit:
+        sys.exit()
+
+
+
+
 try:
     if Var.STRING_SESSION:
-        session = StringSession(str(Var.STRING_SESSION))
         PandaBot = PandaUserbotSession(
-            session=session,
+            PandaSession(Var.STRING_SESSION, LOGS),
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH,
             connection=ConnectionTcpAbridged,
@@ -53,7 +119,7 @@ try:
     if Var.STRING_SESSION2:
         session2 = StringSession(str(Var.STRING_SESSION3))
         PandaBot2 = PandaUserbotSession(
-            session=session2,
+            PandaSession(Var.STRING_SESSION2, LOGS),
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH,
             connection=ConnectionTcpAbridged,
@@ -70,7 +136,7 @@ try:
     if Var.STRING_SESSION3:
         session3 = StringSession(str(Var.STRING_SESSION3))
         PandaBot3 = PandaUserbotSession(
-            session=session3,
+            PandaSession(Var.STRING_SESSION3, LOGS),
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH,
             connection=ConnectionTcpAbridged,
