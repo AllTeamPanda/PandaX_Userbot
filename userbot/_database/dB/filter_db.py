7650ac20b3ea073a31678ad6bfe4c1db1b10b60d
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-02 10:48:24 +0700

    Update filter_db.py

--- userbot/_database/dB/filter_db.py
+++ userbot/_database/dB/filter_db.py
@@ -16,9 +16,9 @@ def get_stuff():
 def add_filter(chat_id, keyword, reply, f_mesg_id):
     ok = get_stuff()
     if ok.get(chat_id):
-        ok[chat_id].update({keyword: {"reply": reply, "f_mesg_id": f_mesg_id}})
+        ok[chat_id].update({str(chat_id): {keyword: {"reply": reply, "f_mesg_id": f_mesg_id}}})
     else:
-        ok.update({chat_id: {keyword: {"reply": reply, "f_mesg_id": f_mesg_id}}})
+        ok.update({str(chat_id): {keyword: {"reply": reply, "f_mesg_id": f_mesg_id}}})
     udB.set_key("FILTERS", ok)
 
 
