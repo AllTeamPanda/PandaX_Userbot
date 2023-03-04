ec25d0052b0ae5bba06cfeb60590cfc054745636
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-04 10:39:38 +0700

    Update botsett.py

--- userbot/modules/telethon/assistant/botsett.py
+++ userbot/modules/telethon/assistant/botsett.py
@@ -80,3 +80,52 @@ async def bot_start(event):
             )
     
 
+
+
+
+@tgbot.bot_cmd(
+    pattern=f"^/help?([\s]+)?$",
+    incoming=True,
+    func=lambda e: e.is_private,
+)
+async def bot_start(event):
+    chat = await event.get_chat()
+    user = await pandaub.get_me()
+    reply_to = await reply_id(event)
+    if chat.id != Config.OWNER_ID:
+        start_msg = f"Hey 🙋 {_format.mentionuser(chat.first_name , chat.id)},\
+                    \nSaya {_format.mentionuser(user.first_name , user.id)}' Assistant_bot.\
+                    \nKamu Bisa Setting saya disini dan Daftar menu perintah Saya sebagai Asistantmu.\
+                    \n\nPowered by @PandaUserbot"
+        buttons = [
+            (
+                Button.inline("🌀 Help 🌀", data="mainmenu"),
+            )
+        ]
+    else:
+        start_msg = f"Hey 🙋 {_format.mentionuser(chat.first_name , chat.id)},\
+                    \nSaya {_format.mentionuser(user.first_name , user.id)}' Assistant_bot.\
+                    \nKamu Bisa Setting saya disini dan Daftar menu perintah Saya sebagai Asistantmu.\
+                    \n\nPowered by @PandaUserbot"
+        buttons = [
+            (
+                Button.inline("🌀 Help 🌀", data="mainmenu"),
+            )
+        ]
+    try:
+        await event.client.send_message(
+            chat.id,
+            start_msg,
+            link_preview=False,
+            buttons=buttons,
+            reply_to=reply_to,
+        )
+    except Exception as e:
+        if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"**Kesalahan**\nTerjadi Kesalahan pada bot saat loading.\
+                \n`{str(e)}`",
+            )
+    
+
