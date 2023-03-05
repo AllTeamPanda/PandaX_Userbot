494e959c08f310c04e02616a80ff61661d26be5e
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-05 18:06:45 +0700

    Update tools.py

--- userbot/_misc/tools.py
+++ userbot/_misc/tools.py
@@ -7,8 +7,8 @@
 
 import re
 from telethon import Button
-
-
+import json
+import requests
 
 def create_tl_btn(button: list):
     btn = []
@@ -52,4 +52,29 @@ def format_btn(buttons: list):
                     txt += f"[{i.button.text} | {i.button.url}]"
     _, btn = get_msg_button(txt)
     return btn
+
+
+
+
+def translate(*args, **kwargs):
+    headers = {
+        "Referer": "https://translate.google.co.in",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/47.0.2526.106 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    x = requests.post(
+        "https://translate.google.co.in/_/TranslateWebserverUi/data/batchexecute",
+        headers=headers,
+        data=_package_rpc(*args, **kwargs),
+    ).text
+    response = ""
+    data = json.loads(json.loads(x[4:])[0][2])[1][0][0]
+    subind = data[-2]
+    if not subind:
+        subind = data[-1]
+    for i in subind:
+        response += i[0]
+    return response
 
