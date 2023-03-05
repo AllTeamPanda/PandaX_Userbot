4309216afed870ce92dd8b67ab4e3196a83cc374
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-05 19:26:35 +0700

    Update string.py

--- pyPanda/string.py
+++ pyPanda/string.py
@@ -1,48 +1,74 @@
 import os
-from glob import glob
+import sys
+from os import listdir, path
 from typing import Any, Dict, List, Union
 
-from userbot import *
-from userbot._misc.tools import translate
+from userbot import LOGS
+try:
+    from google_trans_new import google_translator
+    Trs = google_translator()
+except ImportError:
+    LOGS.error("'google_trans_new' tidak terpasang!")
+    Trs = None
 
 try:
     from yaml import safe_load
 except ModuleNotFoundError:
-    from userbot._misc.tools import safe_load
 
-Plang = udB.get_key("language") or os.getenv("LANGUAGE", "en")
+    def _get_value(stri):
+        try:
+            value = eval(stri.strip())
+        except Exception as er:
+            LOGS.debug(er)
+            value = stri.strip()
+        return value
 
+    def safe_load(file, *args, **kwargs):
+        read = file.readlines()
+        out = {}
+        for line in read:
+            if ":" in line: # Ignores Empty & Invalid lines
+                spli = line.split(":", maxsplit=1)
+                key = spli[0].strip()
+                value = _get_value(spli[1])
+                out.update({key: value or []})
+            elif "-" in line:
+                spli = line.split("-", maxsplit=1)
+                where = out[list(out.keys())[-1]]
+                if isinstance(where, list):
+                    value = _get_value(spli[1])
+                    if value:
+                        where.append(value)
+        return out
+
+language = [os.environ.get("language") or "en"]
 languages = {}
-PATH = "pyPanda/strings/{}.yml"
+
+strings_folder = path.join(path.dirname(path.realpath(__file__)), "strings")
+
+for file in listdir(strings_folder):
+    if file.endswith(".yml"):
+        code = file[:-4]
+        try:
+            languages[code] = safe_load(
+                open(path.join(strings_folder, file), encoding="UTF-8"),
+            )
+        except Exception as er:
+            LOGS.info(f"Kesalahan dalam {file[:-4]} file bahasa")
+            LOGS.exception(er)
 
 
-def load(file):
-    if not file.endswith(".yml"):
-        return
-    elif not os.path.exists(file):
-        file = PATH.format("en")
-    code = file.split("/")[-1].split("\\")[-1][:-4]
-    try:
-        languages[code] = safe_load(
-            open(file, encoding="UTF-8"),
-        )
-    except Exception as er:
-        LOGS.info(f"Error in {file[:-4]} language file")
-        LOGS.exception(er)
-
-
-load(PATH.format(Plang))
-
-
-def get_string(key: str, _res: bool = True) -> Any:
-    lang = Plang or "en"
+def get_string(key: str) -> Any:
+    lang = language[0]
     try:
         return languages[lang][key]
     except KeyError:
         try:
             en_ = languages["en"][key]
-            tr = translate(en_, lang_tgt=lang).replace("\ N", "\n")
-            if en_.count("{}") != tr.count("{}"):
+            if not Trs:
+                return en_
+            tr = Trs.translate(en_, lang_tgt=lang).replace("\ N", "\n")
+            if id_.count("{}") != tr.count("{}"):
                 tr = en_
             if languages.get(lang):
                 languages[lang][key] = tr
@@ -50,30 +76,23 @@ def get_string(key: str, _res: bool = True) -> Any:
                 languages.update({lang: {key: tr}})
             return tr
         except KeyError:
-            if not _res:
-                return
-            return f"Warning: could not load any string with the key `{key}`"
+            return f"Peringatan: tidak dapat memuat string apa pun dengan kunci `{key}`"
         except TypeError:
             pass
         except Exception as er:
             LOGS.exception(er)
-        if not _res:
-            return None
-        return languages["en"].get(key) or f"Failed to load language string '{key}'"
-
+        return languages["en"].get(key) or f"Gagal memuat string bahasa '{key}'"
 
 def get_help(key):
-    get_string(f"help_{key}", _res=False)
+    get_string(f"help_{key}")
     
 
 def get_languages() -> Dict[str, Union[str, List[str]]]:
-    for file in glob("pyPanda/strings/*yml"):
-        load(file)
     return {
         code: {
-            "name": languages[code]["name"],
-            "natively": languages[code]["natively"],
-            "authors": languages[code]["authors"],
+            "nama": languages[code]["nama"],
+            "asli": languages[code]["asli"],
+            "penulis": languages[code]["penulis"],
         }
         for code in languages
     }
