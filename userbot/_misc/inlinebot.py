ece50befc1a2808f340bfe77655d71c9c07e5712
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-05 11:58:42 +0700

    Update inlinebot.py

--- userbot/_misc/inlinebot.py
+++ userbot/_misc/inlinebot.py
@@ -564,7 +564,7 @@ import base64
 @tgbot.on(callbackquery.CallbackQuery(data=re.compile(b"closes")))
 @check_owner
 async def on_plugin_callback_query_handler(event):
-    query = event
+    query = []
     string = query
     if string:
         dc_id, message_id, chat_id, query_id = struct.unpack(
