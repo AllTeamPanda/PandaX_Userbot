547f290c564bd9eb25b2c49955cb72ef3f7c5e7d
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-06 14:51:58 +0700

    Update vcp_helper.py

--- userbot/_helper/VC/vcp_helper.py
+++ userbot/_helper/VC/vcp_helper.py
@@ -56,7 +56,7 @@ class VCTools:
         try:
             await self.app.join_group_call(
                 chat_id=chat.id,
-                stream=AudioPiped("userbot/resources/Silence01s.mp3"),
+                stream=AudioPiped("userbot/resources/resources_Silence01s.mp3"),
                 join_as=join_as_chat,
                 stream_type=StreamType().pulse_stream,
             )
@@ -152,7 +152,7 @@ class VCTools:
             if self.PLAYING:
                 await self.app.change_stream(
                     self.CHAT_ID,
-                    AudioPiped("userbot/resources/Silence01s.mp3"),
+                    AudioPiped("userbot/resources/resources_Silence01s.mp3"),
                 )
             self.PLAYING = False
             return "Skipped Stream\nEmpty Playlist"
