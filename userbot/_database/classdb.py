10316c3311a1ed24219ef6eade84641e7d958128
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-08 00:53:38 +0700

    Update classdb.py

--- userbot/_database/classdb.py
+++ userbot/_database/classdb.py
@@ -1,6 +1,38 @@
 from . import pdB
 
 
+
+
+
+class WELCOME():
+    try:
+        eval(pdB.get_key("WELCOME"))
+    except BaseException:
+        pdB.set_key("WELCOME", "{}")
+
+    def set_welcome(self, chat_id, file_id, text=None):
+        ok = eval(pdB.get_key("WELCOME"))
+        ok.update({chat_id: {"file_id": file_id, "caption": text}})
+        return pdB.set_key("WELCOME", ok)
+
+    def get_welcome(self, chat_id):
+        ok = eval(pdB.get_key("WELCOME"))
+        wl = ok.get(chat_id)
+        if wl:
+            return wl
+        return
+
+
+    def del_welcome(self, chat_id):
+        ok = eval(pdB.get_key("WELCOME"))
+        wl = ok.get(chat_id)
+        if wl:
+            ok.pop(chat_id)
+            return pdB.set_key("WELCOME", ok)
+        return
+
+
+
 class DV():
     setdv = pdB.set_key
     getdv = pdB.get_key
@@ -9,5 +41,5 @@ class DV():
 
 
 
-class Database(DV):
+class Database(DV, WELCOME):
     pass
