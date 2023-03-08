123af1099449690a2209d7ef1e23995620bec280
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-08 21:47:24 +0700

    Update rawfunctions.py

--- userbot/_helper/functions/rawfunctions.py
+++ userbot/_helper/functions/rawfunctions.py
@@ -754,7 +754,41 @@ class RawFunctions(object):
                 print(x + " Loaded !")
         return count
      
-     
+    def get_arg(message):
+        msg = message.text
+        msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+        split = msg[1:].replace("\n", " \n").split(" ")
+        if " ".join(split[1:]).strip() == "":
+            return ""
+        return " ".join(split[1:])
+
+    def get_user(message: Message, text: str) -> [int, str, None]:
+        """Get User From Message"""
+        if text is None:
+            asplit = None
+        else:
+            asplit = text.split(" ", 1)
+        user_s = None
+        reason_ = None
+        if message.reply_to_message:
+            user_s = message.reply_to_message.from_user.id
+            reason_ = text if text else None
+        elif asplit is None:
+            return None, None
+        elif len(asplit[0]) > 0:
+            if message.entities:
+                if len(message.entities) == 1:
+                    required_entity = message.entities[0]
+                    if required_entity.type == "text_mention":
+                        user_s = int(required_entity.user.id)
+                    else:
+                        user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+            else:
+                user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+            if len(asplit) == 2:
+                reason_ = asplit[1]
+        return user_s, reason_
+
     def db_status(self):
         from ..._database import pdB
         """
