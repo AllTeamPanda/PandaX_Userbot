d597c647d1d4de59d7927998d597b50d67227039
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-09 17:32:17 +0700

    Update rawfunctions.py

--- userbot/_helper/functions/rawfunctions.py
+++ userbot/_helper/functions/rawfunctions.py
@@ -762,32 +762,6 @@ class RawFunctions(object):
             return ""
         return " ".join(split[1:])
 
-    def get_user(message: Message, text: str) -> [int, str, None]:
-        """Get User From Message"""
-        if text is None:
-            asplit = None
-        else:
-            asplit = text.split(" ", 1)
-        user_s = None
-        reason_ = None
-        if message.reply_to_message:
-            user_s = message.reply_to_message.from_user.id
-            reason_ = text if text else None
-        elif asplit is None:
-            return None, None
-        elif len(asplit[0]) > 0:
-            if message.entities:
-                if len(message.entities) == 1:
-                    required_entity = message.entities[0]
-                    if required_entity.type == "text_mention":
-                        user_s = int(required_entity.user.id)
-                    else:
-                        user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
-            else:
-                user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
-            if len(asplit) == 2:
-                reason_ = asplit[1]
-        return user_s, reason_
 
     def forward(message, chat_id):
         try:
