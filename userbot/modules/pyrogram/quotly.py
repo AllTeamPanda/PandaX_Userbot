1a6679d4fa38cf7dcb91976cb9e869dc61264b5d
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-09 09:29:49 +0700

    Update quotly.py

--- userbot/modules/pyrogram/quotly.py
+++ userbot/modules/pyrogram/quotly.py
@@ -7,161 +7,8 @@
 
 
 
-all_col = [
-    "Black",
-    "Navy",
-    "DarkBlue",
-    "MediumBlue",
-    "Blue",
-    "DarkGreen",
-    "Green",
-    "Teal",
-    "DarkCyan",
-    "DeepSkyBlue",
-    "DarkTurquoise",
-    "MediumSpringGreen",
-    "Lime",
-    "SpringGreen",
-    "Aqua",
-    "Cyan",
-    "MidnightBlue",
-    "DodgerBlue",
-    "LightSeaGreen",
-    "ForestGreen",
-    "SeaGreen",
-    "DarkSlateGray",
-    "DarkSlateGrey",
-    "LimeGreen",
-    "MediumSeaGreen",
-    "Turquoise",
-    "RoyalBlue",
-    "SteelBlue",
-    "DarkSlateBlue",
-    "MediumTurquoise",
-    "Indigo  ",
-    "DarkOliveGreen",
-    "CadetBlue",
-    "CornflowerBlue",
-    "RebeccaPurple",
-    "MediumAquaMarine",
-    "DimGray",
-    "DimGrey",
-    "SlateBlue",
-    "OliveDrab",
-    "SlateGray",
-    "SlateGrey",
-    "LightSlateGray",
-    "LightSlateGrey",
-    "MediumSlateBlue",
-    "LawnGreen",
-    "Chartreuse",
-    "Aquamarine",
-    "Maroon",
-    "Purple",
-    "Olive",
-    "Gray",
-    "Grey",
-    "SkyBlue",
-    "LightSkyBlue",
-    "BlueViolet",
-    "DarkRed",
-    "DarkMagenta",
-    "SaddleBrown",
-    "DarkSeaGreen",
-    "LightGreen",
-    "MediumPurple",
-    "DarkViolet",
-    "PaleGreen",
-    "DarkOrchid",
-    "YellowGreen",
-    "Sienna",
-    "Brown",
-    "DarkGray",
-    "DarkGrey",
-    "LightBlue",
-    "GreenYellow",
-    "PaleTurquoise",
-    "LightSteelBlue",
-    "PowderBlue",
-    "FireBrick",
-    "DarkGoldenRod",
-    "MediumOrchid",
-    "RosyBrown",
-    "DarkKhaki",
-    "Silver",
-    "MediumVioletRed",
-    "IndianRed ",
-    "Peru",
-    "Chocolate",
-    "Tan",
-    "LightGray",
-    "LightGrey",
-    "Thistle",
-    "Orchid",
-    "GoldenRod",
-    "PaleVioletRed",
-    "Crimson",
-    "Gainsboro",
-    "Plum",
-    "BurlyWood",
-    "LightCyan",
-    "Lavender",
-    "DarkSalmon",
-    "Violet",
-    "PaleGoldenRod",
-    "LightCoral",
-    "Khaki",
-    "AliceBlue",
-    "HoneyDew",
-    "Azure",
-    "SandyBrown",
-    "Wheat",
-    "Beige",
-    "WhiteSmoke",
-    "MintCream",
-    "GhostWhite",
-    "Salmon",
-    "AntiqueWhite",
-    "Linen",
-    "LightGoldenRodYellow",
-    "OldLace",
-    "Red",
-    "Fuchsia",
-    "Magenta",
-    "DeepPink",
-    "OrangeRed",
-    "Tomato",
-    "HotPink",
-    "Coral",
-    "DarkOrange",
-    "LightSalmon",
-    "Orange",
-    "LightPink",
-    "Pink",
-    "Gold",
-    "PeachPuff",
-    "NavajoWhite",
-    "Moccasin",
-    "Bisque",
-    "MistyRose",
-    "BlanchedAlmond",
-    "PapayaWhip",
-    "LavenderBlush",
-    "SeaShell",
-    "Cornsilk",
-    "LemonChiffon",
-    "FloralWhite",
-    "Snow",
-    "Yellow",
-    "LightYellow",
-    "Ivory",
-    "White",
-]
+import asyncio
 
-from os import remove
-from random import choice
-
-from ...helpers.utils.misc import create_quotly
 from pyrogram.types import Message
 
 from ... import app, gen
@@ -173,50 +20,21 @@ app.CMD_HELP.update(
 
 @app.on_message(gen(["q"], allow=["sudo", "channel"]))
 async def quotly_handler(_, m: Message):
-    match = m.text
-    if not m.reply_to_message:
-        return await app.send_edit(m, "**Mohon Balas ke Pesan**")
-    msg = await app.send_edit(m, "`Processing...`")
-    reply = await m.reply_to_message
-    replied_to, reply_ = None, None
-    user__ = reply.from_user.id
-    if match:
-        spli_ = match.split(maxsplit=1)
-        if (spli_[0] in ["r", "reply"]) or (
-            spli_[0].isdigit() and int(spli_[0]) in range(1, 21)
-        ):
-            if spli_[0].isdigit():
-                if not user__.is_bot:
-                    reply_ = await app.get_messages(
-                        m.chat.id,
-                        min_id=event.reply_to_msg_id - 1,
-                        reverse=True,
-                        limit=int(spli_[0]),
-                    )
-                else:
-                    id_ = reply.from_user.id
-                    reply_ = []
-                    for msg_ in range(id_, id_ + int(spli_[0])):
-                        msh = await app.get_messages(m.chat.id, ids=msg_)
-                        if msh:
-                            reply_.append(msh)
-            else:
-                replied_to = await m.reply_to_message
-            try:
-                match = spli_[1]
-            except IndexError:
-                match = None
-    if not reply_:
-        reply_ = reply
-    if match:
-        match = match.split(maxsplit=1)
-    if match == "random":
-        match = choice(all_col)
-    try:
-        file = await create_quotly(reply_, bg=match, reply=replied_to)
-    except Exception as er:
-        return await app.send_edit(m, f"**ERROR:** `{er}`")
-    message = await reply.reply("Quotly by PandaUserbot", file=file)
-    remove(file)
-    await msg.delete()
-    return message
+    reply = m.reply_to_message
+    if not reply:
+        return await app.send_edit(m, "Reply to any users text message", delme=4)
+
+    m = await app.send_edit(m, "Making a Quote . . .", text_type=["mono"])
+    await reply.forward("@QuotLyBot")
+    is_sticker = True
+    while is_sticker:
+        try:
+            msg = await app.get_last_msg(message=m, chat_id="@QuotLyBot", limit=1)
+            msg[0]["sticker"]["file_id"]
+            is_sticker = False
+        except:
+            await asyncio.sleep(0.5)
+    if msg_id := msg[0]["message_id"]:
+        await asyncio.gather(
+            m.delete(), app.copy_message(m.chat.id, "@QuotLyBot", msg_id)
+        )
