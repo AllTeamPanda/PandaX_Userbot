a8456514fd8160fdf7a2062a2ac303600245face
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-09 10:29:53 +0700

    Create startup.py

---
+++ userbot/_func/helper/startup.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import shlex
+
+import ntpath
+import shlex
+from typing import Tuple
+import importlib
+from pathlib import Path
+import os
+
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
