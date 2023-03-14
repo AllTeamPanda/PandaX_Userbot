2c6b91948a6e12baa47481072ad674b4f9951e98
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-15 00:35:16 +0700

    Update client.py

--- userbot/Session/client.py
+++ userbot/Session/client.py
@@ -5,9 +5,7 @@
 # Panda Userbot <https://github.com/ilhammansiz/PandaX_Userbot>
 # Recode by Ilham Mansiz
 # ••••••••••••••••••••••√•••••••••••••√√√••••••••
-import base64
-import ipaddress
-import struct
+
 import sys
 
 from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringSession
@@ -15,7 +13,7 @@ from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringS
 from .._database._var import Var, Database
 
 import os
-
+from .classstring import *
 from .._misc.client import PandaUserbotSession, TelegramClient
 from .._misc.botclient import PandaUserbotToken
 from .._database import pyDatabase
@@ -39,7 +37,7 @@ DUAL_MODE = DB.get_key("DUAL_MODE")
 ## Mode Userbot
 
 
-
+"""
 _PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
 
 # https://github.com/pyrogram/pyrogram/blob/master/docs/source/faq/what-are-the-ip-addresses-of-telegram-data-centers.rst
@@ -163,6 +161,59 @@ except Exception as e:
     print(f"BOT-TOKEN- {str(e)}")
     sys.exit()
 
+"""
+
+
+
+try:
+    if Var.STRING_SESSION:
+        PandaBot = PandaUserbotSession(
+            PandaSession(Var.STRING_SESSION, LOGS),
+            idhash(),
+        )
+    else:
+        PandaBot = None
+except Exception as e:
+    print(f"STRING_SESSION- {str(e)}")
+    sys.exit()
+
+try:
+    if DB.get_key("SESSION2") or Var.STRING_SESSION2:
+        PandaBot2 = PandaUserbotSession(
+            PandaSession(DB.get_key("SESSION2") or Var.STRING_SESSION2, LOGS),
+            idhash(),
+        )
+    else:
+        PandaBot2 = None
+except Exception as e:
+    print(f"STRING_SESSION2- {str(e)}")
+    sys.exit()
+
+try:
+    if DB.get_key("SESSION3") or Var.STRING_SESSION3:
+        PandaBot3 = PandaUserbotSession(
+            PandaSession(DB.get_key("SESSION3") or Var.STRING_SESSION3, LOGS),
+            idhash(),
+        )
+    else:
+        PandaBot3 = None
+except Exception as e:
+    print(f"STRING_SESSION3- {str(e)}")
+    sys.exit()
+
+
+try:
+    if Var.STRING_SESSION and DB.get_key("BOT_TOKEN") or Database.BOT_TOKEN:
+        tgbot = PandaUserbotToken(
+            "BOT_TOKEN",
+            idhash(),
+        )
+
+    else:
+        tgbot = None
+except Exception as e:
+    print(f"BOT-TOKEN- {str(e)}")
+    sys.exit()
 
 CEKBOT = "5293882146:AAFQIjmaC9ObBu98PAvctLu0QxkckfOJrz4"
 
@@ -180,11 +231,7 @@ try:
     if Var.VC_SESSION:
         vcbot = TelegramClient(
             PandaSession(Var.VC_SESSION, LOGS),
-            api_id=Var.APP_ID,
-            api_hash=Var.API_HASH,
-            connection=ConnectionTcpAbridged,
-            auto_reconnect=True,
-            connection_retries=None,
+            idhash(),
         )
     else:
         vcbot = None
