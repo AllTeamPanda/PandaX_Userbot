afed3350516e2be037bc557157c194ccfc95ca2f
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-15 11:03:43 +0700

    Update inlinebot.py

--- userbot/_misc/inlinebot.py
+++ userbot/_misc/inlinebot.py
@@ -81,7 +81,7 @@ def main_menu():
         (
             Button.url(
                 "⚙️ Settings",
-                f"https://t.me/{Config.BOT_USERNAME}?start=menuset",
+                f"https://t.me/{Config.BOT_USERNAME}",
             ),
             Button.inline(
                 f"⚙️ Info",
@@ -119,7 +119,7 @@ def Helpeororr():
         (
             Button.url(
                 "⚙️ Setting",
-                f"https://t.me/{Config.BOT_USERNAME}?start=menuset",
+                f"https://t.me/{Config.BOT_USERNAME}",
             ),
             Button.inline(
                 f"💎 𝙸𝚗𝚏𝚘",
