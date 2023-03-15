26542dfa27025aac86c023ff5fefcb12aafe863d
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-15 09:28:16 +0700

    Delete pyPanda directory

--- pyPanda/queues/__init__.py
+++
@@ -1,6 +0,0 @@
-from .queues import *
-from .misc import *
-from .thumbnail import *
-from .chattitle import *
-from .bash import *
-
--- pyPanda/queues/bash.py
+++
@@ -1,3 +0,0 @@
-from pyPanda.RawUtilities import bash
-
-bash = bash
