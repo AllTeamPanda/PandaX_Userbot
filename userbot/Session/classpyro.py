509aaeeeccda0ea4f59075f098bf9bdaf7b7f0c2
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-16 16:39:50 +0700

    Update classpyro.py

--- userbot/Session/classpyro.py
+++ userbot/Session/classpyro.py
@@ -15,7 +15,7 @@ class PyroClient(Utils, Client):
 
     def __init__(self):
         super().__init__(
-            session_name=Pandapyro(self.PyroSESSION, LOGS)
+            Pandapyro(self.PyroSESSION, LOGS)
             api_id=self.API_ID,
             api_hash=self.API_HASH,
             workers=self.WORKERS,
