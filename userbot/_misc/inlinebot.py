de383df8d24751ace773f3960c729139ecb91342
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-16 08:03:14 +0700

    Update inlinebot.py

--- userbot/_misc/inlinebot.py
+++ userbot/_misc/inlinebot.py
@@ -570,12 +570,12 @@ import base64
 async def on_plugin_callback_query_handler(event):
     if event.data == b'close':
         try:
-            if event.message_id:
+            if event.get_message:
                 dc_id, message_id, chat_id, query_id = struct.unpack(
                     "<iiiq",
                     base64.urlsafe_b64decode(
-                        event.message_id + '=' * (
-                            len(event.message_id) % 4
+                        event.get_message + '=' * (
+                            len(event.get_message) % 4
                         )
                     )
                 )
