267bd197dca4d38c066809eb35e580c538d5ff52
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-17 10:23:53 +0700

    Update base64.py

--- userbot/modules/telethon/base64.py
+++ userbot/modules/telethon/base64.py
@@ -100,3 +100,39 @@ async def encode(event):
         await edit_or_reply(event, "**ERROR :** " + str(p))
 
 
+_PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
+import ipaddress
+import struct
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="destring(?: |$)(.*)",
+    command=("destring", plugin_category),
+    info={
+        "header": "destring ",
+        "usage": "{tr}destring string pyro",
+    },
+)
+async def encode(event):
+    ppk = event.pattern_match.group(1)
+    event.chat.id
+    if len(ppk) in _PYRO_FORM.keys():
+        data_ = struct.unpack(
+                _PYRO_FORM[len(ppk)],
+                base64.urlsafe_b64decode(ppk + "=" * (-len(ppk) % 4)),
+        )
+        if len(ppk) in [351, 356]:
+            auth_id = 2
+        else:
+            auth_id = 3
+
+        dc_id, auth_key = data_[0], data_[auth_id]       
+    try:
+        et = dc_id, aunth_key
+        atc = et
+        await edit_or_reply(event,
+            f"**=>> Decoded Text :** `{data_}`\n\n**=>> OUTPUT :**\n`{atc}`"
+        )
+    except Exception as p:
+        await edit_or_reply(event, "**ERROR :** " + str(p))
+
+
