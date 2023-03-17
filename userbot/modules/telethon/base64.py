cad228d2ae65bb7eda7a69426f983baf166be770
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-17 09:53:06 +0700

    Update base64.py

--- userbot/modules/telethon/base64.py
+++ userbot/modules/telethon/base64.py
@@ -50,3 +50,53 @@ async def encode(event):
         )
     except Exception as p:
         await edit_or_reply(event, "**ERROR :** " + str(p))
+
+
+
+
+
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="enurl(?: |$)(.*)",
+    command=("urlencode", plugin_category),
+    info={
+        "header": "url encode ",
+        "usage": "{tr}en text",
+    },
+)
+async def encode(event):
+    ppk = event.pattern_match.group(1)
+    event.chat.id
+    if not ppk:
+        return await edit_or_reply(event, "`Give me Something to Encode..`")
+    byt = ppk.encode("ascii")
+    et = base64.urlsafe_b64encode(byt)
+    atc = et.decode("ascii")
+    await edit_or_reply(event,
+        f"**=>> Encoded Text :** `{ppk}`\n\n**=>> OUTPUT :**\n`{atc}`"
+    )
+
+@PandaBot.ilhammansiz_cmd(
+    pattern="urlde(?: |$)(.*)",
+    command=("urldecode", plugin_category),
+    info={
+        "header": "urldecode ",
+        "usage": "{tr}urlde text",
+    },
+)
+async def encode(event):
+    ppk = event.pattern_match.group(1)
+    event.chat.id
+    if not ppk:
+        return await edit_or_reply(event, "`Give me Something to Decode..`")
+    byt = ppk.encode("ascii")
+    try:
+        et = base64.urlsafe_b64decode(byt)
+        atc = et.decode("ascii")
+        await edit_or_reply(event,
+            f"**=>> Decoded Text :** `{ppk}`\n\n**=>> OUTPUT :**\n`{atc}`"
+        )
+    except Exception as p:
+        await edit_or_reply(event, "**ERROR :** " + str(p))
+
+
