84686931e39c947079074a352708302b07f8f343
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-19 11:45:34 +0700

    Update start.py

--- exportir/start.py
+++ exportir/start.py
@@ -2,9 +2,10 @@
 # Panda - UserBot
 # Copyright (C) 2022-2023 Panda Userbot
 
-
+from base64 import b64decode
 from time import sleep
 from os import system, path
+from userbot import Config
 
 PANDA = "★ PANDA USERBOT DEPLOY★"
 
@@ -51,11 +52,38 @@ def envbot():
                 file.write(f"{var}={inp}\n")
         sleep(0.1)
         system("screen -S Panda")
-        sleep(0.1)
         
-   
+def Zip():
+    local regex
+    regex='(https?)://github.com/.+/.+'
+    if Config.UPSTREAM_REPO == "PANDA_USERBOT":
+        Zip = "aHR0cHM6Ly9naXRodWIuY29tL1RlYW1YUGFuZGEvUGFuZGEvYXJjaGl2ZS9tYWluLnppcA=="
+        code = Zip.encode()
+        msg = b64decode(code)
+        Url = msg.decode()
+        system("wget Url")
+        system("bash start")
+
+    elif Config.UPSTREAM_REPO =~ regex:
+          if U_BRANCH:
+              Url = f"{Config.UPSTREAM_REPO}/archive/{U_BRANCH}.zip"
+              system("wget Url")
+              system("bash start")      
+          else:
+              Url = f"{Config.UPSTREAM_REPO}/archive/main.zip"
+              system("wget Url")
+              system("bash start")
+    else:
+        Zip = "aHR0cHM6Ly9naXRodWIuY29tL1RlYW1YUGFuZGEvUGFuZGEvYXJjaGl2ZS9tYWluLnppcA=="
+        code = Zip.encode()
+        msg = b64decode(code)
+        msg.decode()
+        Url = msg.decode()
+        system("wget Url")
+        system("bash start")      
+             
 
 if not path.exists(".env"):
     print(Start("Typing y for start env"))
     envbot()
-    print(system("bash start"))
+    print(Zip())
