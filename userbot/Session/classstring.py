9dad23b82bca9369369ca4c886507949c8f958c0
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-19 19:19:21 +0700

    Update classstring.py

--- userbot/Session/classstring.py
+++ userbot/Session/classstring.py
@@ -79,8 +79,6 @@ def PandaSession(session, logger=LOGS, _exit=True):
     if _exit:
         sys.exit()
 
-from pyrogram.storage import MemoryStorage
-pyropanda = MemoryStorage()
 
 def PyroSession(session_name, logger=LOGS, _exit=True):
     if session_name:
@@ -102,12 +100,12 @@ def PyroSession(session_name, logger=LOGS, _exit=True):
         
             packed = struct.pack(
                 SESSION_STRING_FORMAT,
-                pyropanda.dc_id(dc_id),
-                pyropanda.api_id(api_id),
-                pyropanda.test_mode(test_mode),
-                pyropanda.auth_key(auth_key),
-                pyropanda.user_id(user_id),
-                pyropanda.is_bot(is_bot)
+                dc_id,
+                api_id,
+                test_mode,
+                auth_key,
+                user_id,
+                is_bot
             )
             return base64.urlsafe_b64encode(packed).decode().rstrip("=")
         else:
