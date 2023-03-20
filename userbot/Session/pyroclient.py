46ea6a6ebb408582b36cc0dfd05a8b7a8e6ca4a8
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-20 21:15:24 +0700

    Update pyroclient.py

--- userbot/Session/pyroclient.py
+++ userbot/Session/pyroclient.py
@@ -5,12 +5,25 @@
 # Dibuat dari berbagai userbot yang pernah ada.
 # t.me/pandac0de t.me/pandauserbot
 
-
+from pyrogram import Client
 from .classpyro import PyroClient
 from .._database import pdB
 from .._database._var import Database
+from .classstring import *
 
+"""
 if pdB.get_key("PyroSESSION") or Database.PyroSESSION:
     app = PyroClient()
 else:
     app = None
+"""
+
+if pdB.get_key("PyroSESSION") or Database.PyroSESSION:
+    pyrobot = Client(
+        PyroSession(pdB.get_key("PyroSESSION") or self.PyroSESSION, LOGS),
+        api_id=Database.APP_ID,
+        api_hash=Database.API_HASH,
+        sleep_threshold=180,
+    )
+else:
+    pyrobot = None
