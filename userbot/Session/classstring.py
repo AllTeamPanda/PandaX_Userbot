f2f2016aae4ca03cf098c7f5b1aebd575ec8823f
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-03-22 17:56:28 +0700

    Update classstring.py

--- userbot/Session/classstring.py
+++ userbot/Session/classstring.py
@@ -84,7 +84,7 @@ def PyroSession(session_name, logger=LOGS, _exit=True):
                 dc_id, ip, port, key = struct.unpack(
                     _STRUCT_PREFORMAT.format(ip_len), base64.urlsafe_b64decode(ppk))
                 api_id = False
-                test_mode = key
+                test_mode = False
                 auth_key = key
                 user_id = False
                 is_bot = False
