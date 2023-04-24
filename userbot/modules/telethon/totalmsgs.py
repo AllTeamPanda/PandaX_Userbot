2367c61671cceef11a7058b833ad88207c31abc4
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-04-24 19:39:02 +0700

    Update totalmsgs.py

--- userbot/modules/telethon/totalmsgs.py
+++ userbot/modules/telethon/totalmsgs.py
@@ -14,10 +14,10 @@ plugin_category = "plugins"
 async def _(event):
     k = await event.get_reply_message()
     if k:
-        a = await bot.get_messages(event.chat_id, 0, from_user=k.sender_id)
+        a = await event.client.get_messages(event.chat_id, 0, from_user=k.sender_id)
         return await event.edit(f"Total Message Dari {u}. Total Chats `{a.total}`")
     u = event.pattern_match.group(1)
     if not u:
         u = "me"
-    a = await bot.get_messages(event.chat_id, 0, from_user=u)
+    a = await event.client.get_messages(event.chat_id, 0, from_user=u)
     await event.edit(f"Total Message Dari {u}. Total Chats `{a.total}`")
