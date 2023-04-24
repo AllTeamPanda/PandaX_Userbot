2ee889ac50a47e9321e9eca0dbba83c622a9ede0
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-04-24 22:24:13 +0700

    Create autobio_loop.py

---
+++ exportir/RawUtilities/AutoPP/autobio_loop.py
@@ -0,0 +1,16 @@
+from .autopicloop import *
+
+async def autobio_loop():
+    AUTOBIOSTART = gvarstatus("autobio") == "true"
+    while AUTOBIOSTART:
+        DMY = time.strftime("%d.%m.%Y")
+        HM = time.strftime("%H:%M:%S")
+        bio = f"📅 {DMY} | {DEFAULTUSERBIO} | ⌚️ {HM}"
+        LOGS.info(bio)
+        try:
+            await pandaub(functions.account.UpdateProfileRequest(about=bio))
+        except FloodWaitError as ex:
+            LOGS.warning(str(ex))
+            await asyncio.sleep(ex.seconds)
+        await asyncio.sleep(Config.CHANGE_TIME)
+        AUTOBIOSTART = gvarstatus("autobio") == "true"
