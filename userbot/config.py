123338eb7b185bcb99606bfa2ef4529ebe75d9e6
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-05-23 15:23:31 +0700

    Update config.py

--- userbot/config.py
+++ userbot/config.py
@@ -402,13 +402,13 @@ class Config(object):
     ALIVE_IMG = os.environ.get(
         "ALIVE_IMG", "https://telegra.ph/file/8f0e9e25427f9da09e08a.jpg"
     )
-    PLUGINS_REPO = os.environ.get("EXTERNAL_REPO", None)
+    PLUGINS_REPO = os.environ.get("PLUGINS_REPO", "https://github.com/AllTeamPanda/Plugins")
     if bool(PLUGINS_REPO and (PLUGINS_REPO.lower() != "false")):
         if not url(PLUGINS_REPO):
             PLUGINS_REPO = "https://github.com/AllTeamPanda/Plugins"
     else:
         PLUGINS_REPO = None
-    EXTRA_REPO = os.environ.get("EXTERNAL_REPO", None)
+    EXTRA_REPO = os.environ.get("EXTRA_REPO", "https://github.com/AllTeamPanda/Extra")
     if bool(EXTRA_REPO and (EXTRA_REPO.lower() != "false")):
         if not url(EXTRA_REPO):
             EXTRA_REPO = "https://github.com/AllTeamPanda/Extra"
