d253a6be9eba382bd0c67e65ab42fc76d81992c8
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-05-23 11:30:37 +0700

    Update rawfunctions.py

--- exportir/RawUtilities/rawfunctions.py
+++ exportir/RawUtilities/rawfunctions.py
@@ -4,32 +4,62 @@
 # ••••••••••••••••••••••√•••••••••••••√√√••••••••
 
 
-import asyncio
+from asyncio import sleep
 
-from userbot._database import gvarstatus
-
+from telethon.errors import MessageDeleteForbiddenError, MessageNotModifiedError
+from telethon.tl.custom import Message
+from telethon.tl.types import MessageService
 
 # edit or reply
 
 
-async def eor(event, text, **args):
-    link_preview = args.get("link_preview", False)
-    parse_mode = args.get("parse_mode", "md")
-    time = args.get("time", None)
-    if not event.out:
-        if event.is_reply:
-            event = await event.get_reply_message()
-        ok = await event.reply(text, link_preview=link_preview, parse_mode=parse_mode)
+async def eor(event, text=None, time=None, link_preview=False, edit_time=None, **args):
+    reply_to = event.reply_to_msg_id or event
+    if event.out and not isinstance(event, MessageService):
+        if edit_time:
+            await sleep(edit_time)
+        if args.get("file") and not event.media:
+            await event.delete()
+            ok = await event.client.send_message(
+                event.chat_id,
+                text,
+                link_preview=link_preview,
+                reply_to=reply_to,
+                **args
+            )
+        else:
+            try:
+                ok = await event.edit(text, link_preview=link_preview, **args)
+            except MessageNotModifiedError:
+                ok = event
     else:
-        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
-    ut = gvarstatus("DEL_DELAY_TIME")
-    if time and ut != "None":
-        time = time or ut
-        await asyncio.sleep(int(time))
+        ok = await event.client.send_message(
+            event.chat_id, text, link_preview=link_preview, reply_to=reply_to, **args
+        )
+
+    if time:
+        await sleep(time)
         return await ok.delete()
     return ok
 
 
-async def eod(event, text=None, **args):
-    time = args.get("time", 5)
-    return await eor(event, text, time=time)
+async def eod(event, text=None, **kwargs):
+    kwargs["time"] = kwargs.get("time", 8)
+    return await eor(event, text, **kwargs)
+
+
+async def _try_delete(event):
+    try:
+        return await event.delete()
+    except (MessageDeleteForbiddenError):
+        pass
+    except BaseException as er:
+        from . import LOGS
+
+        LOGS.error("Error while Deleting Message..")
+        LOGS.exception(er)
+
+
+setattr(Message, "eor", eor)
+setattr(Message, "try_delete", _try_delete)
+
