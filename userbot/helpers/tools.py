bef3235167f22761c4b0f0b1466542e030c31255
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-05-30 17:55:31 +0700

    Update tools.py

--- userbot/helpers/tools.py
+++ userbot/helpers/tools.py
@@ -1,5 +1,8 @@
 from html_telegraph_poster import TelegraphPoster
-
+from .utils.utils import runcmd
+import contextlib
+import json
+import os
 
 def media_type(message):
     if message and message.photo:
@@ -32,3 +35,26 @@ async def post_to_telegraph(page_title, html_format_content):
         text=html_format_content,
     )
     return post_page["url"]
+
+
+async def fileinfo(file):
+    x, y, z, s = await runcmd(f"mediainfo '{file}' --Output=JSON")
+    cat_json = json.loads(x)["media"]["track"]
+    dic = {
+        "path": file,
+        "size": int(cat_json[0]["FileSize"]),
+        "extension": cat_json[0]["FileExtension"],
+        "type": "None",
+        "format": "None",
+        "audio": "None",
+    }
+    with contextlib.suppress(IndexError, KeyError):
+        dic["format"] = cat_json[0]["Format"]
+        dic["type"] = cat_json[1]["@type"]
+        if "ImageCount" not in cat_json[0]:
+            dic["duration"] = int(float(cat_json[0]["Duration"]))
+            dic["bitrate"] = int(cat_json[0]["OverallBitRate"]) // 1000
+        dic["height"] = int(cat_json[1]["Height"])
+        dic["width"] = int(cat_json[1]["Width"])
+        dic["audio"] = "Present" if cat_json[0]["AudioCount"] else "None"
+    return dic
