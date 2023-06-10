1b60daaea915f0e4a127a2575530ce31767726a1
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-06-10 10:19:35 +0700

    Delete exportir/RawUtilities/VCTools directory

--- exportir/RawUtilities/VCTools/__init__.py
+++
@@ -1,6 +0,0 @@
-from .vcclient import *
-from .function import check_vcassis, sendmsg, vc_player, vc_reply
-from .inlinevc import buttons
-from .stream_helper import Stream
-from .tg_downloader import tg_dl
-
