b2cdc983f91fa5f5b1b97748b83619fda9bec363
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2023-06-10 14:11:26 +0700

    Update clone.py

--- userbot/modules/telethon/clone.py
+++ userbot/modules/telethon/clone.py
@@ -3,6 +3,9 @@ import html
 
 from telethon.tl import functions
 from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.tl.functions.account import UpdateProfileRequest
+from telethon.tl.functions.photos import DeletePhotosRequest, UploadProfilePhotoRequest
 
 from ...config import Config
 from . import (
@@ -31,7 +34,7 @@ DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else ""
 )
 async def _(event):
     "To clone account of mentiond user or replied user"
-    replied_user, error_i_a = await get_user_from_event(event)
+    replied_user, error_i_a = await get_full_user(event)
     if replied_user is None:
         return
     user_id = replied_user.id
@@ -46,9 +49,9 @@ async def _(event):
     if last_name is None:
         last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
     replied_user = await event.client(GetFullUserRequest(replied_user.id))
-    user_bio = first_name
+    user_bio = replied_user.full_user.about
     if user_bio is not None:
-        user_bio = first_name
+        user_bio = replied_user.full_user.about
     await event.client(functions.account.UpdateProfileRequest(first_name=first_name))
     await event.client(functions.account.UpdateProfileRequest(last_name=last_name))
     await event.client(functions.account.UpdateProfileRequest(about=user_bio))
@@ -89,3 +92,58 @@ async def _(event):
         await event.client.send_message(
             BOTLOG_CHATID, f"#REVERT\nSuccesfully reverted back to your profile"
         )
+
+        
+        
+        
+        
+ async def get_full_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.forward.sender_id
+                    or previous_message.forward.channel_id
+                )
+            )
+            return replied_user, None
+        replied_user = await event.client(
+            GetFullUserRequest(previous_message.sender_id)
+        )
+        return replied_user, None
+    else:
+        input_str = None
+        try:
+            input_str = event.pattern_match.group(1)
+        except IndexError as e:
+            return None, e
+        if event.message.entities is not None:
+            mention_entity = event.message.entities
+            probable_user_mention_entity = mention_entity[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            try:
+                user_object = await event.client.get_entity(input_str)
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        elif event.is_private:
+            try:
+                user_id = event.chat_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        else:
+            try:
+                user_object = await event.client.get_entity(int(input_str))
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e      
